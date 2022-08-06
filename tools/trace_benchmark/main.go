42826ce58a7f2a6d8ee2af619d280023f220b2fb
Laurent Querel <laurent.querel@gmail.com>
2022-08-05 17:02:36 -0700

    Add tool to benchmark trace request representations (OTLP vs OTLP Arrow).

---
+++ tools/trace_benchmark/main.go
@@ -0,0 +1,66 @@
+/*
+ * // Copyright The OpenTelemetry Authors
+ * //
+ * // Licensed under the Apache License, Version 2.0 (the "License");
+ * // you may not use this file except in compliance with the License.
+ * // You may obtain a copy of the License at
+ * //
+ * //       http://www.apache.org/licenses/LICENSE-2.0
+ * //
+ * // Unless required by applicable law or agreed to in writing, software
+ * // distributed under the License is distributed on an "AS IS" BASIS,
+ * // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * // See the License for the specific language governing permissions and
+ * // limitations under the License.
+ *
+ */
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"google.golang.org/protobuf/proto"
+
+	v1 "otel-arrow-adapter/api/go.opentelemetry.io/proto/otlp/collector/trace/v1"
+)
+
+var help = flag.Bool("help", false, "Show help")
+
+func main() {
+	// Parse the flag
+	flag.Parse()
+
+	// Usage Demo
+	if *help {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	// Define default input file
+	inputFiles := flag.Args()
+	if len(inputFiles) == 0 {
+		inputFiles = append(inputFiles, "./data/otlp_traces.pb")
+	}
+
+	// Compare the performance for each input file
+	for i := range inputFiles {
+		fmt.Printf("Benchmark '%s'\n", inputFiles[i])
+
+		// Unmarshal the ExportTraceServiceRequest protobuf file.
+		data, err := os.ReadFile(inputFiles[i])
+		if err != nil {
+			log.Fatal("read file:", err)
+		}
+		var otlp v1.ExportTraceServiceRequest
+		if err := proto.Unmarshal(data, &otlp); err != nil {
+			log.Fatal("unmarshal:", err)
+		}
+
+		// Compare the performance between the standard OTLP representation and the OTLP Arrow representation.
+		// ToDo
+	}
+}
