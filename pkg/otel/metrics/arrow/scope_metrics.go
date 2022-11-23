18ddb00c82ce212712a9096c11addbf1fb23b10e
Laurent Querel <l.querel@f5.com>
2022-11-23 14:44:05 -0800

    Extract shared data from pmetric.Metric

--- pkg/otel/metrics/arrow/scope_metrics.go
+++ pkg/otel/metrics/arrow/scope_metrics.go
@@ -6,10 +6,12 @@ import (
 	"github.com/apache/arrow/go/v11/arrow"
 	"github.com/apache/arrow/go/v11/arrow/array"
 	"github.com/apache/arrow/go/v11/arrow/memory"
+	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/pmetric"
 
 	acommon "github.com/f5/otel-arrow-adapter/pkg/otel/common/arrow"
 	"github.com/f5/otel-arrow-adapter/pkg/otel/constants"
+	"github.com/f5/otel-arrow-adapter/pkg/otel/metrics"
 )
 
 // ScopeMetricsDT is the Arrow Data Type describing a scope span.
@@ -111,4 +113,112 @@ func (b *ScopeMetricsBuilder) Release() {
 
 		b.released = true
 	}
+}
+
+type SharedData struct {
+	startTime  *pcommon.Timestamp
+	time       *pcommon.Timestamp
+	attributes *SharedAttributes
+}
+
+func NewSharedDataFrom(metric pmetric.Metric) (sharedData *SharedData, err error) {
+	sharedData = &SharedData{}
+	var dpLen func() int
+	var dpAt func(int) metrics.DataPoint
+
+	switch metric.Type() {
+	case pmetric.MetricTypeGauge:
+		dps := metric.Gauge().DataPoints()
+		dpLen = func() int { return dps.Len() }
+		dpAt = func(i int) metrics.DataPoint { return dps.At(i) }
+	case pmetric.MetricTypeSum:
+		dps := metric.Sum().DataPoints()
+		dpLen = func() int { return dps.Len() }
+		dpAt = func(i int) metrics.DataPoint { return dps.At(i) }
+	case pmetric.MetricTypeHistogram:
+		dps := metric.Histogram().DataPoints()
+		dpLen = func() int { return dps.Len() }
+		dpAt = func(i int) metrics.DataPoint { return dps.At(i) }
+	case pmetric.MetricTypeSummary:
+		dps := metric.Summary().DataPoints()
+		dpLen = func() int { return dps.Len() }
+		dpAt = func(i int) metrics.DataPoint { return dps.At(i) }
+	case pmetric.MetricTypeExponentialHistogram:
+		dps := metric.ExponentialHistogram().DataPoints()
+		dpLen = func() int { return dps.Len() }
+		dpAt = func(i int) metrics.DataPoint { return dps.At(i) }
+	default:
+		err = fmt.Errorf("unknown metric type: %v", metric.Type())
+		return
+	}
+
+	if dpLen() > 0 {
+		initSharedDataFrom(sharedData, dpAt(0))
+		for i := 1; i < dpLen(); i++ {
+			updateSharedDataWith(sharedData, dpAt(i))
+		}
+	}
+
+	return
+}
+
+func initSharedDataFrom(sharedData *SharedData, initDataPoint metrics.DataPoint) {
+	startTime := initDataPoint.StartTimestamp()
+	sharedData.startTime = &startTime
+	time := initDataPoint.Timestamp()
+	sharedData.time = &time
+	sharedData.attributes = NewSharedAttributesFrom(initDataPoint.Attributes())
+}
+
+func updateSharedDataWith(sharedData *SharedData, dp metrics.DataPoint) int {
+	if sharedData.startTime != nil && uint64(*sharedData.startTime) != uint64(dp.StartTimestamp()) {
+		sharedData.startTime = nil
+	}
+	if sharedData.time != nil && uint64(*sharedData.time) != uint64(dp.Timestamp()) {
+		sharedData.time = nil
+	}
+	return sharedData.attributes.IntersectWith(dp.Attributes())
+}
+
+// SharedAttributes is a data structure representing the shared attributes of a set of metrics.
+type SharedAttributes struct {
+	attributes map[string]pcommon.Value
+}
+
+// NewSharedAttributesFrom creates a new SharedAttributes from a [pcommon.Map] of attributes.
+func NewSharedAttributesFrom(attrs pcommon.Map) *SharedAttributes {
+	attributes := make(map[string]pcommon.Value)
+	attrs.Range(func(k string, v pcommon.Value) bool {
+		attributes[k] = v
+		return true
+	})
+	return &SharedAttributes{
+		attributes: attributes,
+	}
+}
+
+// IntersectWith intersects the current SharedAttributes with a [pcommon.Map] of attributes
+// and returns the number of shared attributes after the intersection.
+func (sa *SharedAttributes) IntersectWith(attrs pcommon.Map) int {
+	for k, v := range sa.attributes {
+		if otherV, ok := attrs.Get(k); ok {
+			if !v.Equal(otherV) {
+				delete(sa.attributes, k)
+			}
+		} else {
+			delete(sa.attributes, k)
+		}
+	}
+	return len(sa.attributes)
+}
+
+// Has returns true if the current SharedAttributes has the given attribute.
+func (sa *SharedAttributes) Has(k string) bool {
+	_, ok := sa.attributes[k]
+	return ok
+}
+
+// Len returns the number of attributes in the current SharedAttributes.
+func (sa *SharedAttributes) Len() int {
+	return len(sa.attributes)
 }
---
+++ pkg/otel/metrics/arrow/scope_metrics_test.go
@@ -0,0 +1,146 @@
+package arrow
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/pmetric"
+)
+
+func TestIntersectAttrs(t *testing.T) {
+	t.Parallel()
+
+	initMap := pcommon.NewMap()
+	initMap.PutStr("a", "1")
+	initMap.PutStr("b", "2")
+	initMap.PutInt("c", 3)
+	initMap.PutDouble("d", 4.0)
+	initMap.PutBool("e", true)
+	initMap.PutEmptyBytes("f").Append([]byte("6")...)
+	initMap.PutEmptyMap("g").PutStr("g1", "7")
+	sharedAttrs := NewSharedAttributesFrom(initMap)
+
+	// All attributes in common
+	attrs := pcommon.NewMap()
+	attrs.PutStr("a", "1")
+	attrs.PutStr("b", "2")
+	attrs.PutInt("c", 3)
+	attrs.PutDouble("d", 4.0)
+	attrs.PutBool("e", true)
+	attrs.PutEmptyBytes("f").Append([]byte("6")...)
+	attrs.PutEmptyMap("g").PutStr("g1", "7")
+	sharedAttrsCount := sharedAttrs.IntersectWith(attrs)
+	require.Equal(t, 7, sharedAttrsCount)
+
+	// 1 attribute is missing from attrs
+	attrs = pcommon.NewMap()
+	attrs.PutStr("a", "1")
+	attrs.PutStr("b", "2")
+	attrs.PutInt("c", 3)
+	attrs.PutDouble("d", 4.0)
+	attrs.PutBool("e", true)
+	attrs.PutEmptyBytes("f").Append([]byte("6")...)
+	sharedAttrsCount = sharedAttrs.IntersectWith(attrs)
+	require.Equal(t, 6, sharedAttrsCount)
+	require.False(t, sharedAttrs.Has("g"))
+
+	// 1 attribute has a different
+	attrs = pcommon.NewMap()
+	attrs.PutStr("a", "1")
+	attrs.PutStr("b", "2")
+	attrs.PutInt("c", 3)
+	attrs.PutDouble("d", 4.0)
+	attrs.PutBool("e", false)
+	attrs.PutEmptyBytes("f").Append([]byte("6")...)
+	sharedAttrsCount = sharedAttrs.IntersectWith(attrs)
+	require.Equal(t, 5, sharedAttrsCount)
+	require.False(t, sharedAttrs.Has("e"))
+
+	// 1 attribute is new
+	attrs = pcommon.NewMap()
+	attrs.PutStr("a", "1")
+	attrs.PutStr("b", "2")
+	attrs.PutInt("c", 3)
+	attrs.PutDouble("d", 4.0)
+	attrs.PutEmptyBytes("f").Append([]byte("6")...)
+	attrs.PutBool("h", false)
+	sharedAttrsCount = sharedAttrs.IntersectWith(attrs)
+	require.Equal(t, 5, sharedAttrsCount)
+	require.False(t, sharedAttrs.Has("h"))
+
+	// 1 attribute is new
+	// 1 attribute is missing
+	// 1 attribute has a different value
+	attrs = pcommon.NewMap()
+	attrs.PutStr("a", "1")
+	attrs.PutStr("b", "2")
+	attrs.PutInt("c", 4)
+	attrs.PutEmptyBytes("f").Append([]byte("6")...)
+	attrs.PutBool("h", false)
+	sharedAttrsCount = sharedAttrs.IntersectWith(attrs)
+	require.Equal(t, 3, sharedAttrsCount)
+	require.True(t, sharedAttrs.Has("a"))
+	require.True(t, sharedAttrs.Has("b"))
+	require.True(t, sharedAttrs.Has("f"))
+
+	// No attributes in common
+	attrs = pcommon.NewMap()
+	attrs.PutStr("x", "1")
+	attrs.PutStr("y", "2")
+	sharedAttrsCount = sharedAttrs.IntersectWith(attrs)
+	require.Equal(t, 0, sharedAttrsCount)
+
+	// Empty attributes
+	attrs = pcommon.NewMap()
+	sharedAttrsCount = sharedAttrs.IntersectWith(attrs)
+	require.Equal(t, 0, sharedAttrsCount)
+}
+
+func TestSharedData(t *testing.T) {
+	t.Parallel()
+
+	metric := SystemMemoryUsage(0, 10)
+	sharedData, err := NewSharedDataFrom(metric)
+	require.NoError(t, err)
+	require.NotNil(t, sharedData.startTime)
+	require.NotNil(t, sharedData.time)
+	require.NotNil(t, sharedData.attributes)
+	require.Equal(t, 1, sharedData.attributes.Len())
+}
+
+func SystemMemoryUsage(startTs, currentTs pcommon.Timestamp) pmetric.Metric {
+	metric := pmetric.NewMetric()
+	metric.SetName("system.memory.usage")
+	metric.SetDescription("Bytes of memory in use.")
+	metric.SetUnit("By")
+
+	sum := metric.SetEmptySum()
+	sum.SetAggregationTemporality(pmetric.AggregationTemporalityCumulative)
+	sum.SetIsMonotonic(false)
+
+	points := sum.DataPoints()
+
+	p1 := points.AppendEmpty()
+	p1.Attributes().PutStr("host", "my-host")
+	p1.Attributes().PutStr("state", "used")
+	p1.SetStartTimestamp(startTs)
+	p1.SetTimestamp(currentTs)
+	p1.SetIntValue(10)
+
+	p2 := points.AppendEmpty()
+	p2.Attributes().PutStr("host", "my-host")
+	p2.Attributes().PutStr("state", "free")
+	p2.SetStartTimestamp(startTs)
+	p2.SetTimestamp(currentTs)
+	p2.SetIntValue(20)
+
+	p3 := points.AppendEmpty()
+	p3.Attributes().PutStr("host", "my-host")
+	p3.Attributes().PutStr("state", "inactive")
+	p3.SetStartTimestamp(startTs)
+	p3.SetTimestamp(currentTs)
+	p3.SetIntValue(30)
+
+	return metric
+}
