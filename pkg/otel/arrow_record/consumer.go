6f061e4c13a395bb4744c50557aa8c853ec3484a
Laurent Querel <l.querel@f5.com>
2022-12-09 14:03:42 -0800

    Add comment to Consumer.Consumer to explain the record lifecycle.

--- pkg/otel/arrow_record/consumer.go
+++ pkg/otel/arrow_record/consumer.go
@@ -136,6 +136,7 @@ func (c *Consumer) TracesFrom(bar *colarspb.BatchArrowRecords) ([]ptrace.Traces,
 }
 
 // Consume takes a BatchArrowRecords protobuf message and returns an array of RecordMessage.
+// Note: the records wrapped in the RecordMessage must be released after use by the caller.
 func (c *Consumer) Consume(bar *colarspb.BatchArrowRecords) ([]*RecordMessage, error) {
 
 	var ibes []*RecordMessage
