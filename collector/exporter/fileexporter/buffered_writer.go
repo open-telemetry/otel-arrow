95211c5a139c84f7117905531dd45e2122778f06
Joshua MacDonald <jmacd@users.noreply.github.com>
2023-08-14 15:35:07 -0700

    Remove the gen/ package directory; move components appropriately (#9)
    Part of https://github.com/open-telemetry/otel-arrow/issues/3
    
    The `collector/gen` subdirectory has been used to copy code to and from
    the https://github.com/open-telemetry/otel-arrow-collector repository,
    with one exception (fileexporter) which has been there to benefit from
    the `internal/` code. Everything in the `gen` directory moves one level
    up, in addition to the primary components being renamed:
    
    - `otlpexporter` becomes `otelarrowexporter`
    - `otlpreceiver` becomes `otelarrowreceiver`
    
    All the associated import statements are updated.

---
+++ collector/exporter/fileexporter/buffered_writer.go
@@ -0,0 +1,48 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package fileexporter // import "github.com/open-telemetry/otel-arrow/collector/exporter/fileexporter"
+
+import (
+	"bufio"
+	"io"
+
+	"go.uber.org/multierr"
+)
+
+// bufferedWriteCloser is intended to use more memory
+// in order to optimize writing to disk to help improve performance.
+type bufferedWriteCloser struct {
+	wrapped  io.Closer
+	buffered *bufio.Writer
+}
+
+var (
+	_ io.WriteCloser = (*bufferedWriteCloser)(nil)
+)
+
+func newBufferedWriteCloser(f io.WriteCloser) WriteCloseFlusher {
+	return &bufferedWriteCloser{
+		wrapped:  f,
+		buffered: bufio.NewWriter(f),
+	}
+}
+
+func (bwc *bufferedWriteCloser) Write(p []byte) (n int, err error) {
+	return bwc.buffered.Write(p)
+}
+
+func (bwc *bufferedWriteCloser) Close() error {
+	return multierr.Combine(
+		bwc.buffered.Flush(),
+		bwc.wrapped.Close(),
+	)
+}
+
+func (bwc *bufferedWriteCloser) getWrapped() io.Closer {
+	return bwc.wrapped
+}
+
+func (bwc *bufferedWriteCloser) Flush() error {
+	return bwc.buffered.Flush()
+}
---
+++ collector/exporter/otelarrowexporter/doc.go
@@ -0,0 +1,5 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+// package otelarrowexporter exports data by using the OTLP format to a gPRC endpoint.
+package otelarrowexporter // import "github.com/open-telemetry/otel-arrow/collector/exporter/otelarrowexporter"
