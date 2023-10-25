b35c371fa2d0108f5cfc0eb686a1c7553398e6dc
Joshua MacDonald <josh.macdonald@gmail.com>
2023-10-25 13:37:38 -0700

    revert trace/otlp.go

--- collector/receiver/otelarrowreceiver/internal/trace/otlp.go
+++ collector/receiver/otelarrowreceiver/internal/trace/otlp.go
@@ -6,11 +6,9 @@ package trace // import "github.com/open-telemetry/otel-arrow/collector/receiver
 import (
 	"context"
 
-	"github.com/open-telemetry/otel-arrow/collector/netstats"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/pdata/ptrace/ptraceotlp"
 	"go.opentelemetry.io/collector/receiver/receiverhelper"
-	tracepb "go.opentelemetry.io/proto/otlp/collector/trace/v1"
 )
 
 const dataFormatProtobuf = "protobuf"
@@ -30,12 +28,9 @@ func New(nextConsumer consumer.Traces, obsrecv *receiverhelper.ObsReport) *Recei
 	}
 }
 
-var methodName = netstats.GRPCUnaryMethodName(tracepb.TraceService_ServiceDesc, tracepb.TraceService_ServiceDesc.Methods[0])
-
 // Export implements the service Export traces func.
 func (r *Receiver) Export(ctx context.Context, req ptraceotlp.ExportRequest) (ptraceotlp.ExportResponse, error) {
 	td := req.Traces()
-
 	// We need to ensure that it propagates the receiver name as a tag
 	numSpans := td.SpanCount()
 	if numSpans == 0 {
@@ -46,8 +41,6 @@ func (r *Receiver) Export(ctx context.Context, req ptraceotlp.ExportRequest) (pt
 	err := r.nextConsumer.ConsumeTraces(ctx, td)
 	r.obsrecv.EndTracesOp(ctx, dataFormatProtobuf, numSpans, err)
 
-	// Note: no call to CountSend() here because we know the uncompressed
-	// size of this object is zero until any fields are set, and they're not.
 	return ptraceotlp.NewExportResponse(), err
 }
 
