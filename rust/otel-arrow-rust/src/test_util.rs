f458109b44a3b0d20cf958052923646210ebbf42
Lei, HUANG <6406592+v0y4g3r@users.noreply.github.com>
2025-04-01 02:24:52 +0800

    chore: bump dependecies (#310)
    # Bump dependency versions
    
    • Upgraded arrow to version 54 and opentelemetry-proto to version 0.27
    in Cargo.toml.
     • Updated tonic to version 0.13 and rand to version 0.9.

--- rust/otel-arrow-rust/src/test_util.rs
+++ rust/otel-arrow-rust/src/test_util.rs
@@ -11,13 +11,13 @@
 // limitations under the License.
 
 use arrow::array::{
-    ArrayRef, BinaryArray, BooleanArray, Float32Array, Float64Array,
-    Int16Array, Int32Array, Int64Array, Int8Array, RecordBatch, StringArray,
-    TimestampMicrosecondArray, TimestampMillisecondArray, TimestampNanosecondArray,
-    TimestampSecondArray, UInt16Array, UInt32Array, UInt64Array, UInt8Array,
+    ArrayRef, BinaryArray, BooleanArray, Float32Array, Float64Array, Int16Array, Int32Array,
+    Int64Array, Int8Array, RecordBatch, StringArray, TimestampMicrosecondArray,
+    TimestampMillisecondArray, TimestampNanosecondArray, TimestampSecondArray, UInt16Array,
+    UInt32Array, UInt64Array, UInt8Array,
 };
 use arrow::datatypes::{DataType, Field, Schema, SchemaRef, TimeUnit};
-use rand::distributions::{Alphanumeric, DistString};
+use rand::distr::{Alphanumeric, SampleString};
 use rand::Rng;
 use std::sync::Arc;
 
@@ -39,56 +39,56 @@ pub(crate) fn create_record_batch(schema: SchemaRef, num_rows: usize) -> RecordB
 }
 
 pub(crate) fn create_array(dt: &DataType, num_rows: usize) -> ArrayRef {
-    let mut r = rand::thread_rng();
+    let mut r = rand::rng();
     match dt {
         DataType::Boolean => Arc::new(
             (0..num_rows)
-                .map(|_| Some(r.gen_bool(1.0 / 2.0)))
+                .map(|_| Some(r.random_bool(1.0 / 2.0)))
                 .collect::<BooleanArray>(),
         ) as Arc<_>,
-        DataType::Int8 => {
-            Arc::new(Int8Array::from_iter((0..num_rows).map(|_| r.gen::<i8>()))) as Arc<_>
-        }
-        DataType::Int16 => {
-            Arc::new(Int16Array::from_iter((0..num_rows).map(|_| r.gen::<i16>()))) as Arc<_>
-        }
-        DataType::Int32 => {
-            Arc::new(Int32Array::from_iter((0..num_rows).map(|_| r.gen::<i32>()))) as Arc<_>
-        }
-        DataType::Int64 => {
-            Arc::new(Int64Array::from_iter((0..num_rows).map(|_| r.gen::<i64>()))) as Arc<_>
-        }
-        DataType::UInt8 => {
-            Arc::new(UInt8Array::from_iter((0..num_rows).map(|_| r.gen::<u8>()))) as Arc<_>
-        }
+        DataType::Int8 => Arc::new(Int8Array::from_iter(
+            (0..num_rows).map(|_| r.random::<i8>()),
+        )) as Arc<_>,
+        DataType::Int16 => Arc::new(Int16Array::from_iter(
+            (0..num_rows).map(|_| r.random::<i16>()),
+        )) as Arc<_>,
+        DataType::Int32 => Arc::new(Int32Array::from_iter(
+            (0..num_rows).map(|_| r.random::<i32>()),
+        )) as Arc<_>,
+        DataType::Int64 => Arc::new(Int64Array::from_iter(
+            (0..num_rows).map(|_| r.random::<i64>()),
+        )) as Arc<_>,
+        DataType::UInt8 => Arc::new(UInt8Array::from_iter(
+            (0..num_rows).map(|_| r.random::<u8>()),
+        )) as Arc<_>,
         DataType::UInt16 => Arc::new(UInt16Array::from_iter(
-            (0..num_rows).map(|_| r.gen::<u16>()),
+            (0..num_rows).map(|_| r.random::<u16>()),
         )) as Arc<_>,
         DataType::UInt32 => Arc::new(UInt32Array::from_iter(
-            (0..num_rows).map(|_| r.gen::<u32>()),
+            (0..num_rows).map(|_| r.random::<u32>()),
         )) as Arc<_>,
         DataType::UInt64 => Arc::new(UInt64Array::from_iter(
-            (0..num_rows).map(|_| r.gen::<u64>()),
+            (0..num_rows).map(|_| r.random::<u64>()),
         )) as Arc<_>,
         DataType::Float32 => Arc::new(Float32Array::from_iter(
-            (0..num_rows).map(|_| r.gen::<f32>()),
+            (0..num_rows).map(|_| r.random::<f32>()),
         )) as Arc<_>,
         DataType::Float64 => Arc::new(Float64Array::from_iter(
-            (0..num_rows).map(|_| r.gen::<f64>()),
+            (0..num_rows).map(|_| r.random::<f64>()),
         )) as Arc<_>,
         DataType::Timestamp(unit, _) => match unit {
             TimeUnit::Second => Arc::new(TimestampSecondArray::from_iter(&Int64Array::from_iter(
-                (0..num_rows).map(|_| r.gen::<i64>()),
+                (0..num_rows).map(|_| r.random::<i64>()),
             ))) as Arc<_>,
             TimeUnit::Millisecond => Arc::new(TimestampMillisecondArray::from_iter(
-                &Int64Array::from_iter((0..num_rows).map(|_| r.gen::<i64>())),
+                &Int64Array::from_iter((0..num_rows).map(|_| r.random::<i64>())),
             )) as Arc<_>,
             TimeUnit::Microsecond => Arc::new(TimestampMicrosecondArray::from_iter(
-                &Int64Array::from_iter((0..num_rows).map(|_| r.gen::<i64>())),
+                &Int64Array::from_iter((0..num_rows).map(|_| r.random::<i64>())),
             )) as Arc<_>,
 
             TimeUnit::Nanosecond => Arc::new(TimestampNanosecondArray::from_iter(
-                &Int64Array::from_iter((0..num_rows).map(|_| r.gen::<i64>())),
+                &Int64Array::from_iter((0..num_rows).map(|_| r.random::<i64>())),
             )) as Arc<_>,
         },
         DataType::Binary | DataType::LargeBinary => Arc::new(BinaryArray::from_iter(
