076ff5d9c8d688fb43043ee77aff347c425169ba
c1ly <c.ly@f5.com>
2025-04-16 16:07:22 -0700

    create otlp receiver struct inside lib.rs implimenting the receiver trait. Defined grpc.rs to create necessary services for grpc server
    create grpc server services and OTLP receiver implimenting the receiver trait
    
    move grpc_stubs

---
+++ rust/otap-dataflow/crates/otlp/src/grpc.rs
@@ -0,0 +1,150 @@
+use grpc_stubs::proto::collector::logs::v1::logs_service_server::{LogsService, LogsServiceServer};
+use grpc_stubs::proto::collector::logs::v1::{ExportLogsServiceRequest, ExportLogsServiceResponse};
+use grpc_stubs::proto::collector::metrics::v1::metrics_service_server::{
+    MetricsService, MetricsServiceServer,
+};
+use grpc_stubs::proto::collector::metrics::v1::{
+    ExportMetricsServiceRequest, ExportMetricsServiceResponse,
+};
+use grpc_stubs::proto::collector::trace::v1::trace_service_server::{
+    TraceService, TraceServiceServer,
+};
+use grpc_stubs::proto::collector::trace::v1::{
+    ExportTraceServiceRequest, ExportTraceServiceResponse,
+};
+use tonic::{Request, Response, Status};
+use otap_df_engine::receiver::EffectHandler;
+
+/// Expose the OTLP gRPC services.
+/// See the build.rs file for more information.
+pub mod grpc_stubs {
+    #[path = ""]
+    pub mod proto {
+        #[path = ""]
+        pub mod collector {
+            #[path = ""]
+            pub mod logs {
+                #[allow(unused_qualifications)]
+                #[allow(unused_results)]
+                #[allow(clippy::enum_variant_names)]
+                #[allow(rustdoc::invalid_html_tags)]
+                #[path = "opentelemetry.proto.collector.logs.v1.rs"]
+                pub mod v1;
+            }
+            #[path = ""]
+            pub mod metrics {
+                #[allow(unused_qualifications)]
+                #[allow(unused_results)]
+                #[allow(clippy::enum_variant_names)]
+                #[allow(rustdoc::invalid_html_tags)]
+                #[path = "opentelemetry.proto.collector.metrics.v1.rs"]
+                pub mod v1;
+            }
+            #[path = ""]
+            pub mod trace {
+                #[allow(unused_qualifications)]
+                #[allow(unused_results)]
+                #[allow(clippy::enum_variant_names)]
+                #[allow(rustdoc::invalid_html_tags)]
+                #[path = "opentelemetry.proto.collector.trace.v1.rs"]
+                pub mod v1;
+            }
+        }
+
+        #[path = ""]
+        pub mod logs {
+            #[allow(rustdoc::invalid_html_tags)]
+            #[path = "opentelemetry.proto.logs.v1.rs"]
+            pub mod v1;
+        }
+
+        #[path = ""]
+        pub mod metrics {
+            #[allow(rustdoc::invalid_html_tags)]
+            #[path = "opentelemetry.proto.metrics.v1.rs"]
+            pub mod v1;
+        }
+
+        #[path = ""]
+        pub mod trace {
+            #[allow(rustdoc::invalid_html_tags)]
+            #[path = "opentelemetry.proto.trace.v1.rs"]
+            pub mod v1;
+        }
+
+        #[path = ""]
+        pub mod common {
+            #[allow(clippy::enum_variant_names)]
+            #[path = "opentelemetry.proto.common.v1.rs"]
+            pub mod v1;
+        }
+
+        #[path = ""]
+        pub mod resource {
+            #[path = "opentelemetry.proto.resource.v1.rs"]
+            pub mod v1;
+        }
+    }
+}
+
+
+pub struct LogsServiceImpl {
+    effect_handler: EffectHandler<OTLPRequest>,
+}
+pub struct MetricsServiceImpl {
+    effect_handler: EffectHandler<OTLPRequest>,
+}
+pub struct TraceServiceImpl {
+    effect_handler: EffectHandler<OTLPRequest>,
+}
+
+#[tonic::async_trait]
+impl LogsService for LogsServiceImpl {
+    async fn export(
+        &self,
+        request: Request<ExportLogsServiceRequest>,
+    ) -> Result<Response<ExportLogsServiceResponse>, Status> {
+
+        self.effect_handler.send_message(OTLPRequest::Logs(request.into_inner())).await?;
+        Ok(Response::new(ExportLogsServiceResponse {
+            partial_success: None,
+        }))
+    }
+}
+
+#[tonic::async_trait]
+impl MetricsService for MetricsServiceImpl {
+    async fn export(
+        &self,
+        request: Request<ExportMetricsServiceRequest>,
+    ) -> Result<Response<ExportMetricsServiceResponse>, Status> {
+
+        self.effect_handler.send_message(OTLPRequest::Metrics(request.into_inner())).await?;
+        Ok(Response::new(ExportMetricsServiceResponse {
+            partial_success: None,
+        }))
+    }
+}
+
+#[tonic::async_trait]
+impl TraceService for TraceServiceImpl {
+    async fn export(
+        &self,
+        request: Request<ExportTraceServiceRequest>,
+    ) -> Result<Response<ExportTraceServiceResponse>, Status> {
+  
+        self.effect_handler.send_message(OTLPRequest::Traces(request.into_inner())).await?;
+        Ok(Response::new(ExportTraceServiceResponse {
+            partial_success: None,
+        }))
+    }
+}
+
+// Enum to represent received OTLP requests.
+#[derive(Debug)]
+pub enum OTLPRequest {
+    Logs(ExportLogsServiceRequest),
+    Metrics(ExportMetricsServiceRequest),
+    Traces(ExportTraceServiceRequest),
+}
+
--- rust/otap-dataflow/crates/otlp/src/lib.rs
+++ rust/otap-dataflow/crates/otlp/src/lib.rs
@@ -1,3 +1,83 @@
 // SPDX-License-Identifier: Apache-2.0
 
 //! Implementation of the OTLP nodes (receiver, exporter, processor).
+
+pub mod grpc;
+use crate::grpc::{LogsServiceImpl, MetricsServiceImpl, TraceServiceImpl, OTLPRequest};
+use grpc_stubs::proto::collector::logs::v1::logs_service_server::LogsServiceServer;
+use grpc_stubs::proto::collector::metrics::v1::metrics_service_server::MetricsServiceServer;
+use grpc_stubs::proto::collector::trace::v1::trace_service_server::TraceServiceServer;
+use otap_df_engine::receiver::{EffectHandler, Receiver, ControlMsgChannel};
+use otap_df_engine::error::Error;
+use otap_df_engine::message::ControlMsg;
+use async_trait::async_trait;
+use std::net::SocketAddr;
+use tokio::time::{Duration, sleep};
+use tonic::codec::CompressionEncoding;
+use tonic::transport::Server;
+
+struct OTLPReceiver {
+    listening_addr: SocketAddr,
+}
+
+#[async_trait(?Send)]
+impl Receiver for OTLPReceiver {
+    type Msg = OTLPRequest;
+    async fn start(
+        self: Box<Self>,
+        ctrl_msg_recv: ControlMsgChannel,
+        effect_handler: EffectHandler<Self::Msg>,
+    ) -> Result<(), Error<Self::Msg>> {
+
+        // create listener on addr provided from config
+        let listener = effect_handler.tcp_listener(self.listening_addr)?;
+
+        //create services for the grpc server and clone the effect handler to pass message
+        let logs_service = LogsServiceImpl {
+            effect_handler: effect_handler.clone(),
+        };
+        let metrics_service = MetricsServiceImpl {
+            effect_handler: effect_handler.clone(),
+        };
+        let trace_service = TraceServiceImpl {
+            effect_handler: effect_handler.clone(),
+        };
+
+        //start event loop
+        loop {
+            tokio::select! {
+                // Process an internal event.
+                ctrl_msg = ctrl_msg_recv.recv() => {
+                    match ctrl_msg {
+                        Ok(ControlMsg::Shutdown {reason}) => {
+                            // break event loop
+                            break;
+                        },
+                        Err(e) => {
+                            return Err(Error::ChannelRecvError(e));
+                        }
+                        _ => {
+                            // unknown control message do nothing
+                            ()
+                        }
+                    }
+                }
+                // handle grpc server events
+                grpc_server = Server::builder()
+                .add_service(LogsServiceServer::new(logs_service).send_compressed(CompressionEncoding::Zstd).accept_compressed(CompressionEncoding::Zstd))
+                .add_service(MetricsServiceServer::new(metrics_service).send_compressed(CompressionEncoding::Zstd).accept_compressed(CompressionEncoding::Zstd))
+                .add_service(TraceServiceServer::new(trace_service).send_compressed(CompressionEncoding::Zstd).accept_compressed(CompressionEncoding::Zstd))
+                .serve(listener) => {
+                    grpc_server.await?;
+                }
+                // A timeout branch in case no events occur.
+                () = sleep(Duration::from_secs(1)) => {
+                    // wait for next event
+                }
+            }
+        }
+        //Exit event loop
+        Ok(())
+    }
+
+}
