7f09d719e78cd4d834e9479973014000c2c3990c
c1ly <c.ly@f5.com>
2025-04-30 19:50:17 -0700

    Moved protobuf otlp definitions inside the otlp crate under otap-dataflow

--- rust/otap-dataflow/crates/otlp/src/lib.rs
+++ rust/otap-dataflow/crates/otlp/src/lib.rs
@@ -1,3 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 //! Implementation of the OTLP nodes (receiver, exporter, processor).
+
+/// gRPC service implementation
+pub mod grpc;
+/// Generated protobuf files
+pub mod grpc_stubs;
