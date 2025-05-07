016d1e61d1ce544049b9340293b2addcfbea6763
c1ly <c.ly@f5.com>
2025-05-07 11:32:19 -0700

    ran cargo fmt

--- rust/otap-dataflow/crates/otlp/src/grpc.rs
+++ rust/otap-dataflow/crates/otlp/src/grpc.rs
@@ -1,10 +1,8 @@
-
 use crate::grpc_stubs::proto::collector::logs::v1::ExportLogsServiceRequest;
 use crate::grpc_stubs::proto::collector::metrics::v1::ExportMetricsServiceRequest;
 use crate::grpc_stubs::proto::collector::trace::v1::ExportTraceServiceRequest;
 
 /// Expose the OTLP gRPC services.
-
 
 // Enum to represent received OTLP requests.
 #[derive(Debug, Clone)]
