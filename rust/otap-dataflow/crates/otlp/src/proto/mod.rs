2612194bc5434916125a55a873dc1d53b2f1d23e
c1ly <129437996+c1ly@users.noreply.github.com>
2025-05-08 15:59:50 -0700

    add otlp protobuf files for rust based OTLP pipeline development  (#359)
    Add the required protobuf files and built .rs files using tonic that is
    needed for the OTLP pipeline development in the OTAP-Dataflow crate
    
    ---------
    
    Co-authored-by: Laurent Quérel <l.querel@f5.com>
    Co-authored-by: Drew Relmas <drewrelmas@gmail.com>

---
+++ rust/otap-dataflow/crates/otlp/src/proto/mod.rs
@@ -0,0 +1,68 @@
+#[allow(missing_docs)]
+#[path = ""]
+pub mod opentelemetry {
+    #[path = ""]
+    pub mod collector {
+        #[path = ""]
+        pub mod logs {
+            #[allow(unused_qualifications)]
+            #[allow(unused_results)]
+            #[allow(clippy::enum_variant_names)]
+            #[allow(rustdoc::invalid_html_tags)]
+            #[path = "opentelemetry.proto.collector.logs.v1.rs"]
+            pub mod v1;
+        }
+        #[path = ""]
+        pub mod metrics {
+            #[allow(unused_qualifications)]
+            #[allow(unused_results)]
+            #[allow(clippy::enum_variant_names)]
+            #[allow(rustdoc::invalid_html_tags)]
+            #[path = "opentelemetry.proto.collector.metrics.v1.rs"]
+            pub mod v1;
+        }
+        #[path = ""]
+        pub mod trace {
+            #[allow(unused_qualifications)]
+            #[allow(unused_results)]
+            #[allow(clippy::enum_variant_names)]
+            #[allow(rustdoc::invalid_html_tags)]
+            #[path = "opentelemetry.proto.collector.trace.v1.rs"]
+            pub mod v1;
+        }
+    }
+
+    #[path = ""]
+    pub mod logs {
+        #[allow(rustdoc::invalid_html_tags)]
+        #[path = "opentelemetry.proto.logs.v1.rs"]
+        pub mod v1;
+    }
+
+    #[path = ""]
+    pub mod metrics {
+        #[allow(rustdoc::invalid_html_tags)]
+        #[path = "opentelemetry.proto.metrics.v1.rs"]
+        pub mod v1;
+    }
+
+    #[path = ""]
+    pub mod trace {
+        #[allow(rustdoc::invalid_html_tags)]
+        #[path = "opentelemetry.proto.trace.v1.rs"]
+        pub mod v1;
+    }
+
+    #[path = ""]
+    pub mod common {
+        #[allow(clippy::enum_variant_names)]
+        #[path = "opentelemetry.proto.common.v1.rs"]
+        pub mod v1;
+    }
+
+    #[path = ""]
+    pub mod resource {
+        #[path = "opentelemetry.proto.resource.v1.rs"]
+        pub mod v1;
+    }
+}
