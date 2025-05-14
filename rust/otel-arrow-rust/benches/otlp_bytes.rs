dce46202d654393e878961cc07880e60a58bb801
Joshua MacDonald <josh.macdonald@gmail.com>
2025-05-14 12:18:22 -0700

    missing docs allowed

--- rust/otel-arrow-rust/benches/otlp_bytes.rs
+++ rust/otel-arrow-rust/benches/otlp_bytes.rs
@@ -10,6 +10,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![allow(missing_docs)]
+
 //! This crate benchmarks OTLP and OTAP.
 
 use criterion::{Criterion, black_box, criterion_group, criterion_main};
