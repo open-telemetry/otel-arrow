b1b25f81cf955a70b579d48bfe3d9bc229988ad5
c1ly <c.ly@f5.com>
2025-05-28 15:08:23 -0700

    moved report logic to helper function

--- rust/otap-dataflow/crates/perf_exporter/src/perf_exporter.rs
+++ rust/otap-dataflow/crates/perf_exporter/src/perf_exporter.rs
@@ -1,14 +1,22 @@
-use std::time::{Duration, Instant};
+// SPDX-License-Identifier: Apache-2.0
+
+//! Implementation of the OTLP exporter node
+//!
+//! ToDo: Handle Ack and Nack messages in the pipeline
+//! ToDo: Handle configuratin changes
+//! ToDo: Implement proper deadline function for Shutdown ctrl msg
 
 use async_trait::async_trait;
-use beaubourg::{
-    exporter::{effect::EffectHandler, AsyncExporter, ConcurrencyModel, EngineHandler, Error, ExporterBuilder},
-    signal::{Signal, SignalReceiver},
-};
+use otap_df_engine::error::Error;
+use otap_df_engine::local::exporter as local;
+use otap_df_engine::message::{ControlMsg, Message, MessageChannel};
+use otap_df_otlp::grpc::OTLPData;
 use byte_unit::Byte;
-use message::Message;
+use std::borrow::Cow;
+use std::time::Instant;
 use serde::Deserialize;
-use serde_yaml::Value;
+
+
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
@@ -54,10 +62,8 @@ fn default_io_usage() -> bool {
     true
 }
 
-pub struct PerfExporter {
-    name: String,
+struct PerfExporter {
     config: Config,
-
     received_msg_count: usize,
     received_evt_count: usize,
     total_received_msg_count: usize,
@@ -68,29 +74,54 @@ pub struct PerfExporter {
     meter: Option<self_meter::Meter>,
 }
 
-#[async_trait]
-impl AsyncExporter<Message> for PerfExporter {
-    async fn init(&mut self, engine_handler: &mut EngineHandler) -> Result<(), Error> {
-        self.last_perf_time = Instant::now();
-        engine_handler.timer(Duration::from_millis(self.config.timeout));
-        Ok(())
-    }
+impl PerfExporter {
 
-    async fn export(
-        &mut self,
-        mut signal_receiver: SignalReceiver<Message>,
-        _effect_handler: EffectHandler<Message>,
-    ) -> Result<(), Error> {
+    pub fn new(meter: Option<self_meter::Meter>, config: Config) -> Self {
+        PerfExporter {
+            received_msg_count: 0,
+            received_evt_count: 0,
+            total_received_msg_count: 0,
+            total_received_evt_count: 0,
+            last_perf_time: Instant::now(),
+            meter: meter,
+            config: config
+        }
+    }
+}
+
+#[async_trait(?Send)]
+impl local::Exporter<OTLPData> for PerfExporter {
+    async fn start(
+        self: Box<Self>,
+        mut msg_chan: MessageChannel<OTLPData>,
+        effect_handler: local::EffectHandler<OTLPData>,
+    ) -> Result<(), Error<OTLPData>> {
+        // Loop until a Shutdown event is received.
         loop {
-            match signal_receiver.recv().await {
-                Signal::TimerTick { .. } => {
+            match msg_chan.recv().await? {
+                Message::Control(ControlMsg::TimerTick { .. }) => {
+
+                    /*
+                    Calculates the time elapsed since the last report
+                    Computes message and event throughput (items per second)
+                    Formats performance statistics
+                    If the meter is enabled, collects system resource usage
+                    Prints all metrics to the console
+                    Resets counters for the next interval
+                    */
+                    // get delta time
                     let now = Instant::now();
                     let duration = now - self.last_perf_time;
+
+                    // Formatted output messages for the report
+                    // generate msgs
                     let msg_throughput = format!(
                         "message throughput     : {:.2} msg/s",
                         (self.received_msg_count as f64 / duration.as_secs_f64())
                     );
+                    // TODO HANDLE LATENCY
                     let msg_latency = "message average latency: TBD".to_string();
+
                     let msg_received = format!("total message received : {}", self.total_received_msg_count);
                     let evt_throughput = format!(
                         "event   throughput     : {:.2} evt/s",
@@ -98,201 +129,160 @@ impl AsyncExporter<Message> for PerfExporter {
                     );
                     let evt_received = format!("total event received   : {}", self.total_received_evt_count);
 
-                    if let Some(meter) = self.meter.as_mut() {
-                        meter.scan().map_err(|e| eprintln!("self-meter scan error: {}", e)).ok();
-                        let report = meter.report();
 
+                    if let Some(meter) = self.meter.as_mut() {
+                        // measure resource usage using self_meter package
+                        meter.scan().map_err(|error| Error::ExporterError {
+                            exporter: effect_handler.exporter_name(),
+                            error: format!("Failed to get meter report: {:?}", error),
+                        });
+                        // get report of resource usage
+                        let report = meter.report();
+                        let thread_reports = meter.thread_report();
+                        
+                        // if meter is enabled report it
                         match report {
                             Some(report) => {
-                                println!(
-                                    "{}\n\t- {}\n\t- {}\n\t- {}\n\t- {}\n\t- {}",
-                                    self.name, evt_throughput, evt_received, msg_throughput, msg_latency, msg_received
-                                );
-                                if self.config.cpu_usage {
-                                    println!(
-                                        "\t- global cpu usage       : {}% (100% is all cores)",
-                                        report.global_cpu_usage
-                                    );
-                                    println!(
-                                        "\t- process cpu usage      : {}% (100% is a single core)",
-                                        report.process_cpu_usage
-                                    );
-                                }
-                                if self.config.mem_usage {
-                                    println!(
-                                        "\t- memory rss             : {}",
-                                        Byte::from_bytes(report.memory_rss as u128).get_appropriate_unit(false)
-                                    );
-                                    println!(
-                                        "\t- memory virtual         : {}",
-                                        Byte::from_bytes(report.memory_virtual as u128).get_appropriate_unit(false)
-                                    );
-                                    println!(
-                                        "\t- memory swap            : {}",
-                                        Byte::from_bytes(report.memory_swap as u128).get_appropriate_unit(false)
-                                    );
-                                    println!(
-                                        "\t- memory rss peak        : {}",
-                                        Byte::from_bytes(report.memory_rss_peak as u128).get_appropriate_unit(false)
-                                    );
-                                    println!(
-                                        "\t- memory virtual peak    : {}",
-                                        Byte::from_bytes(report.memory_virtual_peak as u128)
-                                            .get_appropriate_unit(false)
-                                    );
-                                    println!(
-                                        "\t- memory swap peak       : {}",
-                                        Byte::from_bytes(report.memory_swap_peak as u128).get_appropriate_unit(false)
-                                    );
-                                }
-                                if self.config.disk_usage {
-                                    println!(
-                                        "\t- disk read              : {}/s",
-                                        Byte::from_bytes(report.disk_read as u128).get_appropriate_unit(false)
-                                    );
-                                    println!(
-                                        "\t- disk write             : {}/s",
-                                        Byte::from_bytes(report.disk_write as u128).get_appropriate_unit(false)
-                                    );
-                                    println!(
-                                        "\t- disk cancelled         : {}/s",
-                                        Byte::from_bytes(report.disk_cancelled as u128).get_appropriate_unit(false)
-                                    );
-                                }
-                                if self.config.io_usage {
-                                    println!(
-                                        "\t- io read                : {}/s",
-                                        Byte::from_bytes(report.io_read as u128).get_appropriate_unit(false)
-                                    );
-                                    println!(
-                                        "\t- io write               : {}/s",
-                                        Byte::from_bytes(report.io_write as u128).get_appropriate_unit(false)
-                                    );
-                                    println!("\t- io read ops            : {} read/s", report.io_read_ops);
-                                    println!("\t- io write ops           : {} write/s", report.io_write_ops);
-                                }
+                                display_report_default(effect_handler.exporter_name(), &msg_throughput, &msg_latency, &msg_received, &evt_throughput, &evt_received);
+                                display_report_meter(&self.config.cpu_usage, &self.config.mem_usage, &self.config.disk_usage, &self.config.io_usage, &report)
                             }
+                            // failed to get report
                             None => {
-                                println!(
-                                    "{}\n\t- {}\n\t- {}\n\t- {}\n\t- {}\n\t- {}",
-                                    self.name, msg_throughput, msg_latency, msg_received, evt_throughput, evt_received
-                                );
+                                display_report_default(effect_handler.exporter_name(), &msg_throughput, &msg_latency, &msg_received, &evt_throughput, &evt_received);
                             }
                         }
+                    // no meter enabled    
                     } else {
-                        println!(
-                            "{}\n\t- {}\n\t- {}\n\t- {}\n\t- {}\n\t- {}",
-                            self.name, msg_throughput, msg_latency, msg_received, evt_throughput, evt_received
-                        );
+                        display_report_default(effect_handler.exporter_name(), &msg_throughput, &msg_latency, &msg_received, &evt_throughput, &evt_received);
                     }
 
-                    // println!("Threads: {:#?}",
-                    //          self.meter.thread_report().map(|x| x.collect::<BTreeMap<_,_>>()));
 
+                    // reset received counters
+                    // update last_perf_time
                     self.received_msg_count = 0;
                     self.received_evt_count = 0;
                     self.last_perf_time = now;
                 }
-                Signal::Messages { messages } => {
-                    self.received_msg_count += messages.len();
-                    self.total_received_msg_count += messages.len();
+                Message::Control(ControlMsg::Config { .. }) => {
 
-                    let event_count = messages.iter().fold(0, |acc, msg| {
-                        // Compute the size of the main table for the message
-                        let main_table_size = msg
-                            .columnar_tables
-                            .iter()
-                            .find(|table| table.r#type.is_main_table())
-                            .map(|table| table.record_batch.num_rows())
-                            .unwrap_or(0);
+                }
+                Message::Control(ControlMsg::Shutdown { .. }) => {
 
-                        acc + main_table_size
-                    });
+                    break;
+                }
+                Message::PData(message) => {
+                    // increment counters for received messages
+                    match message {
+                        
+                    }
 
+                    // increment counters for received events
                     self.received_evt_count += event_count;
                     self.total_received_evt_count += event_count;
                 }
-                Signal::Stop => return Ok(()),
-                signal => {
-                    return Err(Error::UnsupportedEvent {
-                        exporter: self.name.clone(),
-                        signal: signal.to_string(),
-                    })
+                _ => {
+                    return Err(Error::ExporterError {
+                        exporter: effect_handler.exporter_name().to_owned(),
+                        error: "Unknown control message".to_owned(),
+                    });
                 }
             }
         }
+        Ok(())
     }
 }
 
-pub struct PerfExporterBuilder {
-    name: String,
-    concurrency_model: ConcurrencyModel,
-    config: Value,
+fn count_events_metrics (req: Resource) -> usize {
+    req.iter().fold
 }
 
-impl PerfExporterBuilder {
-    pub fn new(name: String, config: Value) -> Self {
-        let concurrency_model = match config.get("concurrency_model") {
-            None => ConcurrencyModel::TaskPerCore(1),
-            Some(value) => serde_yaml::from_value(value.clone()).expect("failed to deserialize concurrency model"),
-        };
+fn count_events_logs() -> usize {
 
-        PerfExporterBuilder {
-            name,
-            concurrency_model,
-            config,
-        }
+}
+
+fn count_events_trace() -> usize {
+
+}
+
+fn count_events_profiles() -> usize {
+
+}
+
+fn display_report_meter(cpu_usage: &bool, mem_usage: &bool, disk_usage: &bool, io_usage: &bool, report: &self_meter::Report) {
+     if *cpu_usage {
+        // print all cpu usage data 
+        println!(
+            "\t- global cpu usage       : {}% (100% is all cores)",
+            report.global_cpu_usage
+        );
+        println!(
+            "\t- process cpu usage      : {}% (100% is a single core)",
+            report.process_cpu_usage
+        );
+        println!(
+            "\t- gross threads usage  : {}% (100% is a single core)",
+            report.gross_cpu_usage
+        );
+    }
+    if *mem_usage {
+        // print all memory usage data
+        println!(
+            "\t- memory rss             : {}",
+            Byte::from_bytes(report.memory_rss as u128).get_appropriate_unit(false)
+        );
+        println!(
+            "\t- memory virtual         : {}",
+            Byte::from_bytes(report.memory_virtual as u128).get_appropriate_unit(false)
+        );
+        println!(
+            "\t- memory swap            : {}",
+            Byte::from_bytes(report.memory_swap as u128).get_appropriate_unit(false)
+        );
+        println!(
+            "\t- memory rss peak        : {}",
+            Byte::from_bytes(report.memory_rss_peak as u128).get_appropriate_unit(false)
+        );
+        println!(
+            "\t- memory virtual peak    : {}",
+            Byte::from_bytes(report.memory_virtual_peak as u128)
+                .get_appropriate_unit(false)
+        );
+        println!(
+            "\t- memory swap peak       : {}",
+            Byte::from_bytes(report.memory_swap_peak as u128).get_appropriate_unit(false)
+        );
+    }
+    if *disk_usage {
+        println!(
+            "\t- disk read              : {}/s",
+            Byte::from_bytes(report.disk_read as u128).get_appropriate_unit(false)
+        );
+        println!(
+            "\t- disk write             : {}/s",
+            Byte::from_bytes(report.disk_write as u128).get_appropriate_unit(false)
+        );
+        println!(
+            "\t- disk cancelled         : {}/s",
+            Byte::from_bytes(report.disk_cancelled as u128).get_appropriate_unit(false)
+        );
+    }
+    if *io_usage {
+        println!(
+            "\t- io read                : {}/s",
+            Byte::from_bytes(report.io_read as u128).get_appropriate_unit(false)
+        );
+        println!(
+            "\t- io write               : {}/s",
+            Byte::from_bytes(report.io_write as u128).get_appropriate_unit(false)
+        );
+        println!("\t- io read ops            : {} read/s", report.io_read_ops);
+        println!("\t- io write ops           : {} write/s", report.io_write_ops);
     }
 }
 
-impl ExporterBuilder<Message> for PerfExporterBuilder {
-    fn name(&self) -> String {
-        self.name.clone()
-    }
-
-    fn r#type(&self) -> String {
-        "perf".into()
-    }
-
-    fn concurrency_model(&self) -> ConcurrencyModel {
-        ConcurrencyModel::Singleton
-    }
-
-    fn build(&self) -> Result<Box<dyn AsyncExporter<Message> + Send + Sync>, Error> {
-        tracing::info!(exporter_name=%self.name, concurrency_model=?self.concurrency_model, "Building perf exporter");
-        let mut config: Config = serde_yaml::from_value(self.config.clone()).map_err(|e| Error::InvalidConfig {
-            message: e.to_string(),
-            exporter: self.name.clone(),
-            line: e.location().map(|loc| loc.line()),
-            column: e.location().map(|loc| loc.column()),
-        })?;
-        if !config.cpu_usage && !config.mem_usage && !config.disk_usage && !config.io_usage {
-            config.self_usage = false;
-        }
-        let scan_interval_ms = config.timeout;
-        let self_usage = config.self_usage;
-
-        let exporter = Box::new(PerfExporter {
-            name: self.name.clone(),
-            config,
-            received_msg_count: 0,
-            received_evt_count: 0,
-            total_received_msg_count: 0,
-            total_received_evt_count: 0,
-            last_perf_time: Instant::now(),
-            meter: if self_usage {
-                match self_meter::Meter::new(Duration::from_millis(scan_interval_ms)) {
-                    Ok(meter) => Some(meter),
-                    Err(err) => {
-                        eprintln!("self-meter not created (reason: {:?})", err);
-                        None
-                    }
-                }
-            } else {
-                None
-            },
-        });
-
-        Ok(exporter as Box<dyn AsyncExporter<Message> + Send + Sync>)
-    }
+fn display_report_default(name: Cow<'static, str>, msg_throughput: &str, msg_latency: &str, msg_received: &str, evt_throughput: &str, evt_received: &str) {
+    println!(
+        "{}\n\t- {}\n\t- {}\n\t- {}\n\t- {}\n\t- {}",
+        name, msg_throughput, msg_latency, msg_received, evt_throughput, evt_received
+    );
 }
