f3298439f6bab8644a87df04a445c99ef2423f96
github-action-benchmark <github@users.noreply.github.com>
2025-05-29 22:49:12 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for bcdf4b6d1771e1996ca63d00cf8e5ba2240d92b9

---
+++ docs/benchmarks/pipeline-perf/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1748558952938,
+  "repoUrl": "https://github.com/open-telemetry/otel-arrow",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "cijo.thomas@gmail.com",
+            "name": "Cijo Thomas",
+            "username": "cijothomas"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bcdf4b6d1771e1996ca63d00cf8e5ba2240d92b9",
+          "message": "PerfTest - keep track of perf results using gh action (#491)\n\nDraft to figure out permissions etc.",
+          "timestamp": "2025-05-29T15:46:24-07:00",
+          "tree_id": "841b13867eb70b84c488b12512f44e1ebc68790d",
+          "url": "https://github.com/open-telemetry/otel-arrow/commit/bcdf4b6d1771e1996ca63d00cf8e5ba2240d92b9"
+        },
+        "date": 1748558951296,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pipeline-perf-collector-config-throughput",
+            "value": 435666.6666666667,
+            "unit": "logs/sec"
+          },
+          {
+            "name": "pipeline-perf-collector-config-with-batch-processor-throughput",
+            "value": 462833.3333333333,
+            "unit": "logs/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
