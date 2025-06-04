c86d0400b2a2ab4ffca16f421010314dc5d4e32c
gouslu <gouslu@microsoft.com>
2025-06-04 08:40:39 -0700

    update the documentation

--- tools/pipeline_perf_test/orchestrator/orchestrator.py
+++ tools/pipeline_perf_test/orchestrator/orchestrator.py
@@ -49,25 +49,24 @@ Pre-requisites:
         --k8s-loadgen-manifest load_generator/loadgen-manifest.yaml \
         --k8s-namespace perf-test-otel --duration 30
     
-    NOTES: 
-        - If you are using a local kind cluster, you can import the images into the cluster by running:
-        
-            kind load docker-image otel-loadgen --name kind
-            kind load docker-image backend-service --name kind
+    If you are using a local kind cluster you will need to import the images into the cluster by running:
+    
+        kind load docker-image otel-loadgen --name kind
+        kind load docker-image backend-service --name kind
 
-        - And setup the metrics server by running:
+    If the cluster does not have the metrics server installed, you will need to install it. Be aware that the script below also enables insecure TLS for dev purposes:
 
-            # Apply metrics-server
-            kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+        # Apply metrics-server
+        kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 
-            # Patch with both required flags
-            kubectl patch deployment metrics-server -n kube-system --type=json -p='[
-            {"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"--kubelet-insecure-tls"},
-            {"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"--kubelet-preferred-address-types=InternalIP"}
-            ]'
+        # Patch with both required flags
+        kubectl patch deployment metrics-server -n kube-system --type=json -p='[
+        {"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"--kubelet-insecure-tls"},
+        {"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"--kubelet-preferred-address-types=InternalIP"}
+        ]'
 
-            # Restart deployment
-            kubectl rollout restart deployment metrics-server -n kube-system
+        # Restart deployment
+        kubectl rollout restart deployment metrics-server -n kube-system
 """
 import argparse
 import os
