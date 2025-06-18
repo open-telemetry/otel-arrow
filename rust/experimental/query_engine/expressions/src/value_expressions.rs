10c34ff66bcf24e14320d10bfe0f416bf5a6a80a
Mikel Blanchard <mblanchard@macrosssoftware.com>
2025-06-18 15:31:59 -0700

    [query-engine] Rename Clear -> ClearKeys and add doc comments (#617)
    Follow-up to
    https://github.com/open-telemetry/otel-arrow/pull/615#discussion_r2155476636
    
    ## Changes
    
    * Add doc comments where it was missing on expression definitions
    * Rename Clear -> ClearKeys to fit in better with RemoveKeys

--- rust/experimental/query_engine/expressions/src/value_expressions.rs
+++ rust/experimental/query_engine/expressions/src/value_expressions.rs
@@ -2,12 +2,21 @@ use crate::{ScalarExpression, SourceScalarExpression, VariableScalarExpression};
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum MutableValueExpression {
+    /// Source value.
+    ///
+    /// Note: Source may refer to the source itself (root) or data on the source
+    /// accessed via [`crate::ValueAccessor`] selectors.
     Source(SourceScalarExpression),
 
+    /// Variable value.
+    ///
+    /// Note: Variable may refer to the variable itself (root) or data on the
+    /// variable accessed via [`crate::ValueAccessor`] selectors.
     Variable(VariableScalarExpression),
 }
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum ImmutableValueExpression {
+    /// Scalar value.
     Scalar(ScalarExpression),
 }
