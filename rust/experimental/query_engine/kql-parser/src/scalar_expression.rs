7ad12f21843337484cb7eee8ce6448b53d209f70
Mikel Blanchard <mblanchard@macrosssoftware.com>
2025-06-24 11:51:46 -0700

    Lint

--- rust/experimental/query_engine/kql-parser/src/scalar_expression.rs
+++ rust/experimental/query_engine/kql-parser/src/scalar_expression.rs
@@ -3,8 +3,8 @@ use data_engine_parser_abstractions::*;
 use pest::iterators::Pair;
 
 use crate::{
-    Rule, logical_expressions::parse_logical_expression, scalar_conditional_function_expressions::*,
-    scalar_primitive_expressions::*,
+    Rule, logical_expressions::parse_logical_expression,
+    scalar_conditional_function_expressions::*, scalar_primitive_expressions::*,
 };
 
 pub(crate) fn parse_scalar_expression(
