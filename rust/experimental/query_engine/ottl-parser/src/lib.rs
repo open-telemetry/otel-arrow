2f63df173a57f8850ad48d800199bac2dc920c8c
Drew Relmas <drewrelmas@gmail.com>
2025-07-09 13:01:25 -0700

    [query-engine] Add support for OTTL string, int, and float literals (#688)
    A rough duplicate of Copilot's attempt in #612, addressing #611. Doing
    manually because of EasyCLA issues with Copilot authored commits.

--- rust/experimental/query_engine/ottl-parser/src/lib.rs
+++ rust/experimental/query_engine/ottl-parser/src/lib.rs
@@ -1,4 +1,6 @@
 pub(crate) mod ottl_parser;
+pub(crate) mod scalar_expression;
+pub(crate) mod scalar_primitive_expression;
 
 pub use data_engine_parser_abstractions::parse_standard_bool_literal;
 pub use ottl_parser::*;
