db93198ed46967e749523cf68aae75c7c25d69d9
Mikel Blanchard <mblanchard@macrosssoftware.com>
2025-07-10 08:52:21 -0700

    [query-engine] Re-export Parser API in parser crates (#707)
    ## Changes
    
    * Re-export `Parser` & `ParserError` in kql and ottl parser crates
    
    ## Details
    
    We have this
    [Parser](https://github.com/open-telemetry/otel-arrow/blob/4f55ba7eb4abfe5f57177d15603df0e86b23f47a/rust/experimental/query_engine/parser-abstractions/src/parser.rs#L7)
    trait which is the entry point for parsing KQL or OTTL queries.
    
    In order to call these traits users have to bring them into scope which
    currently means depending on the `parser-abstractions` crate. What this
    change does is makes them part of the public API in the KQL & OTTL
    crates so users don't need to worry about `parser-abstractions`.
    
    I feel like this is a good thing to do but I'm also new to Rust 😄 It may
    be something which gets reverted later but I thought it was worth trying
    out.

--- rust/experimental/query_engine/ottl-parser/src/lib.rs
+++ rust/experimental/query_engine/ottl-parser/src/lib.rs
@@ -4,3 +4,8 @@ pub(crate) mod scalar_primitive_expression;
 
 pub use data_engine_parser_abstractions::parse_standard_bool_literal;
 pub use ottl_parser::*;
+
+// Note: Re-export Parser API surface so users don't need to also depend on
+// parser-abstractions crate just to parse queries.
+pub use data_engine_parser_abstractions::Parser;
+pub use data_engine_parser_abstractions::ParserError;
