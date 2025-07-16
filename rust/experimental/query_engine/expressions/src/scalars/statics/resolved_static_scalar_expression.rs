355bb2062244b28d9147478ea7991710faaedad3
Mikel Blanchard <mblanchard@macrosssoftware.com>
2025-07-16 10:54:57 -0700

    Refactor static resolution API.

---
+++ rust/experimental/query_engine/expressions/src/scalars/statics/resolved_static_scalar_expression.rs
@@ -0,0 +1,31 @@
+use crate::{StaticScalarExpression, Value, ValueType};
+
+#[derive(Debug)]
+pub enum ResolvedStaticScalarExpression<'a> {
+    Reference(&'a StaticScalarExpression),
+    Value(StaticScalarExpression),
+}
+
+impl ResolvedStaticScalarExpression<'_> {
+    pub fn get_value_type(&self) -> ValueType {
+        match self {
+            ResolvedStaticScalarExpression::Reference(s) => s.get_value_type(),
+            ResolvedStaticScalarExpression::Value(s) => s.get_value_type(),
+        }
+    }
+
+    pub fn to_value(&self) -> Value {
+        match self {
+            ResolvedStaticScalarExpression::Reference(s) => s.to_value(),
+            ResolvedStaticScalarExpression::Value(s) => s.to_value(),
+        }
+    }
+
+    #[cfg(test)]
+    pub fn as_ref(&self) -> &StaticScalarExpression {
+        match self {
+            ResolvedStaticScalarExpression::Reference(s) => s,
+            ResolvedStaticScalarExpression::Value(s) => s,
+        }
+    }
+}
