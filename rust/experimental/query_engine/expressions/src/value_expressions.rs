1f6ed64b7bc733758f02357bed65ddd55a79504b
Mikel Blanchard <mblanchard@macrosssoftware.com>
2025-07-18 12:54:38 -0700

    [query-engine] Value expression tweaks (#729)
    ## Changes
    
    * Adds missing `Expression` impl for `MutableValueExpression` &
    `ImmutableValueExpression`
    * Implements `PartialEq` for `Value` which calls into `are_values_equal`

--- rust/experimental/query_engine/expressions/src/value_expressions.rs
+++ rust/experimental/query_engine/expressions/src/value_expressions.rs
@@ -1,4 +1,6 @@
-use crate::{ScalarExpression, SourceScalarExpression, VariableScalarExpression};
+use crate::{
+    Expression, QueryLocation, ScalarExpression, SourceScalarExpression, VariableScalarExpression,
+};
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum MutableValueExpression {
@@ -15,8 +17,38 @@ pub enum MutableValueExpression {
     Variable(VariableScalarExpression),
 }
 
+impl Expression for MutableValueExpression {
+    fn get_query_location(&self) -> &QueryLocation {
+        match self {
+            MutableValueExpression::Source(s) => s.get_query_location(),
+            MutableValueExpression::Variable(v) => v.get_query_location(),
+        }
+    }
+
+    fn get_name(&self) -> &'static str {
+        match self {
+            MutableValueExpression::Source(_) => "MutableValueExpression(Source)",
+            MutableValueExpression::Variable(_) => "MutableValueExpression(Variable)",
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum ImmutableValueExpression {
     /// Scalar value.
     Scalar(ScalarExpression),
+}
+
+impl Expression for ImmutableValueExpression {
+    fn get_query_location(&self) -> &QueryLocation {
+        match self {
+            ImmutableValueExpression::Scalar(s) => s.get_query_location(),
+        }
+    }
+
+    fn get_name(&self) -> &'static str {
+        match self {
+            ImmutableValueExpression::Scalar(_) => "ImmutableValueExpression(Scalar)",
+        }
+    }
 }
