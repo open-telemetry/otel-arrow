805946b75a734c778d1f32664fa62876c5ec249e
Mikel Blanchard <mblanchard@macrosssoftware.com>
2025-07-22 11:49:01 -0700

    [query-engine] Add layout for new recordset engine and primitives (#747)
    ## Changes
    
    * Adds a new project engine-recordset2
    * Adds primitives used by the engine to execute expressions
    
    ## Details
    
    The current recordset engine is really a proof-of-concept. It contains
    its own expression tree and everything is tied deeply to the
    implementation. This new engine works more or less in the same way
    except it run expressions from the dedicated project. The goal is to
    build this up and then remove the poc version.

---
+++ rust/experimental/query_engine/engine-recordset2/src/primitives/mod.rs
@@ -0,0 +1,9 @@
+pub(crate) mod owned_value;
+pub(crate) mod resolved_value;
+pub(crate) mod value_mut;
+pub(crate) mod value_storage;
+
+pub use owned_value::*;
+pub use resolved_value::*;
+pub use value_mut::*;
+pub use value_storage::*;
