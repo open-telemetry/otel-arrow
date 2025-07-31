db40f82148e26671dd5d000a5dbe20630489549e
albertlockett <a.lockett@f5.com>
2025-07-31 14:38:19 -0300

    Use new `OtapPata` type in `ParquetExporter` (#810)
    Fixes https://github.com/open-telemetry/otel-arrow/issues/809
    
    Use the new `OtapPData` type in `ParquetExporter`. Before this it was
    generic over anything that could produce `OtapArrowRecords` & `batch_id:
    i64`. This change enables us to use create the `ExporterFactory` for
    this component.
    
    Also, it was unnecessary to have the `batch_id` in the pdata. For
    `ParquetExporter`, this was just being used as a high watermark when
    figuring out which record batches the writer should flush, so it's
    sufficient to just use a locally incrementing counter (since the writer
    is also local to the `start` method).
    
    Also adds some test for deserializing config. These kind of serve as an
    example for how the JSON config should look (it's sometimes not always
    clear, especially when it comes to enum variants).
    
    ---------
    
    Co-authored-by: Laurent Quérel <l.querel@f5.com>

--- rust/otap-dataflow/crates/otap/src/parquet_exporter/config.rs
+++ rust/otap-dataflow/crates/otap/src/parquet_exporter/config.rs
@@ -1,7 +1,11 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+use serde::Deserialize;
+
 /// Configuration of parquet exporter
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(deny_unknown_fields)]
 pub struct Config {
     /// The base URI for where the parquet files should be written
     pub base_uri: String,
@@ -13,7 +17,7 @@ pub struct Config {
     pub writer_options: Option<WriterOptions>,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug, Deserialize, PartialEq)]
 pub struct WriterOptions {
     /// Target number of rows in one parquet file. The writer will flush automatically any files
     /// that attain greater than this number of rows. If this is None, the writer won't flush
@@ -34,7 +38,57 @@ impl Default for WriterOptions {
 }
 
 /// Configuration options for how the parquet files should be partitioned
+#[derive(Debug, Deserialize, PartialEq)]
 pub enum PartitioningStrategy {
     /// compute partition values from schema metadata keys
+    #[serde(alias = "schema_metadata")]
     SchemaMetadata(Vec<String>),
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_deserialize() {
+        let json_cfg = "{
+            \"base_uri\": \"s3://albert-bucket/parquet-files\",
+            \"partitioning_strategies\": [
+                {
+                    \"schema_metadata\": [ \"_part_id\" ]
+                }
+            ],
+            \"writer_options\": {
+                \"target_rows_per_file\": 1000000000
+            }
+        }";
+
+        let config: Config = serde_json::from_str(json_cfg).unwrap();
+        let expected = Config {
+            base_uri: "s3://albert-bucket/parquet-files".to_string(),
+            partitioning_strategies: Some(vec![PartitioningStrategy::SchemaMetadata(vec![
+                "_part_id".to_string(),
+            ])]),
+            writer_options: Some(WriterOptions {
+                target_rows_per_file: Some(1000000000),
+            }),
+        };
+        assert_eq!(config, expected)
+    }
+
+    #[test]
+    fn test_deserialize_error_unknown_fields() {
+        // this has a mistake in it where target_rows_per_file should be
+        // nested w/in writer_options:
+        let json_cfg = "{
+            \"base_uri\": \"s3://albert-bucket/parquet-files\",
+            \"partitioning_strategies\": [
+                {
+                    \"schema_metadata\": [ \"_part_id\" ]
+                }
+            ],
+            \"target_rows_per_file\": 1000000000
+        }";
+        assert!(serde_json::from_str::<Config>(json_cfg).is_err())
+    }
 }
