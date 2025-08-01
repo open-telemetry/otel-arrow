f1c93177e5cc520901bfc6770c3336b8828d9ba8
Mikel Blanchard <mblanchard@macrosssoftware.com>
2025-08-01 10:30:05 -0700

    [query-engine] Remove recordset engine PoC (#846)
    Relates to #747
    
    ## Changes
    
    * Deletes the recordset engine PoC
    
    Note: I'm going to rename engine-recordset2 -> engine-recordset next but
    I wanted to do it as two PRs to (hopefully) have cleaner history.

--- rust/experimental/query_engine/engine-recordset/src/data/data_record_batch.rs
+++
@@ -1,9 +0,0 @@
-use core::fmt::Debug;
-
-use crate::{data_engine::DataEngineState, error::Error};
-
-pub trait DataRecordBatch<T>: Debug {
-    fn drain<S: DataEngineState, F>(&mut self, state: &mut S, action: F) -> Result<(), Error>
-    where
-        F: Fn(&mut S, T) -> Result<(), Error>;
-}
--- rust/experimental/query_engine/engine-recordset/src/macros.rs
+++
@@ -1,5 +0,0 @@
-macro_rules! either {
-    ($test:expr => $true_expr:expr; $false_expr:expr) => {
-        if $test { $true_expr } else { $false_expr }
-    };
-}
