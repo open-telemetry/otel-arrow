da41eaf584bb1c195e95f49ebc48ac012ab42a6c
Mikel Blanchard <mblanchard@macrosssoftware.com>
2025-08-04 14:37:40 -0700

    Add test.

--- rust/experimental/query_engine/expressions/src/scalars/statics/array_scalar_expression.rs
+++ rust/experimental/query_engine/expressions/src/scalars/statics/array_scalar_expression.rs
@@ -55,3 +55,60 @@ impl ArrayValue for ArrayScalarExpression {
         true
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn test_get_item_range() {
+        let test = |range: ArrayRange, expected: &[i64]| {
+            let array = ArrayScalarExpression::new(
+                QueryLocation::new_fake(),
+                vec![
+                    StaticScalarExpression::Integer(IntegerScalarExpression::new(
+                        QueryLocation::new_fake(),
+                        0,
+                    )),
+                    StaticScalarExpression::Integer(IntegerScalarExpression::new(
+                        QueryLocation::new_fake(),
+                        1,
+                    )),
+                    StaticScalarExpression::Integer(IntegerScalarExpression::new(
+                        QueryLocation::new_fake(),
+                        2,
+                    )),
+                    StaticScalarExpression::Integer(IntegerScalarExpression::new(
+                        QueryLocation::new_fake(),
+                        3,
+                    )),
+                    StaticScalarExpression::Integer(IntegerScalarExpression::new(
+                        QueryLocation::new_fake(),
+                        4,
+                    )),
+                ],
+            );
+
+            let mut indices = Vec::new();
+
+            array.get_item_range(
+                range,
+                &mut IndexValueClosureCallback::new(|_, v| {
+                    if let Value::Integer(i) = v {
+                        indices.push(i.get_value());
+                    }
+                    true
+                }),
+            );
+
+            assert_eq!(expected, indices);
+        };
+
+        test((..).into(), &[0, 1, 2, 3, 4]);
+        test((1..).into(), &[1, 2, 3, 4]);
+        test((1..1).into(), &[]);
+        test((..1).into(), &[0]);
+        test((..=1).into(), &[0, 1]);
+        test((1..=1).into(), &[1]);
+    }
+}
