6272d6d9f364b0244d4e479cb6653454f488fe35
albertlockett <a.lockett@f5.com>
2025-08-06 14:07:50 -0400

    fixup code after rebase

--- rust/otap-dataflow/crates/otap/src/fake_data_generator.rs
+++ rust/otap-dataflow/crates/otap/src/fake_data_generator.rs
@@ -2,8 +2,11 @@
 
 //! A fake data generator receiver.
 //! Note: This receiver will be replaced in the future with a more sophisticated implementation.
+//!
 
-use crate::grpc::OtapArrowBytes;
+use std::rc::Rc;
+use std::time::Duration;
+
 use crate::pdata::{OtapPdata, OtlpProtoBytes};
 use crate::{OTAP_RECEIVER_FACTORIES, pdata};
 use async_trait::async_trait;
@@ -14,14 +17,11 @@ use otap_df_engine::error::Error;
 use otap_df_engine::local::receiver as local;
 use otap_df_engine::receiver::ReceiverWrapper;
 use otap_df_engine::{ReceiverFactory, control::ControlMsg};
-use otap_df_otlp::fake_signal_receiver::config::{Config, OTLPSignal};
-use otap_df_otlp::fake_signal_receiver::receiver::generate_signals;
+use otap_df_otlp::fake_signal_receiver::config::{Config, ScenarioStep, SignalConfig};
 use otap_df_otlp::grpc::OTLPData;
-use otel_arrow_rust::proto::opentelemetry::arrow::v1::BatchArrowRecords;
 use prost::{EncodeError, Message};
-use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use std::rc::Rc;
+use tokio::time::sleep;
 
 /// The URN for the fake data generator receiver
 pub const OTAP_FAKE_DATA_GENERATOR_URN: &str = "urn:otel:otap:fake_data_generator";
@@ -95,7 +95,7 @@ impl local::Receiver<OtapPdata> for FakeGeneratorReceiver {
                     }
                 }
                 // run scenario based on provided configuration
-                _ = run_scenario(&self.config, effect_handler.clone()) => {
+                _ = run_scenario(&self.config.get_steps(), effect_handler.clone()) => {
                     // do nothing
                 }
 
@@ -106,21 +106,35 @@ impl local::Receiver<OtapPdata> for FakeGeneratorReceiver {
     }
 }
 
+/// Run the configured scenario steps
 async fn run_scenario(
-    config: &Config,
+    steps: &Vec<ScenarioStep>,
     effect_handler: local::EffectHandler<OtapPdata>,
 ) -> Result<(), Error<OtapPdata>> {
-    for signal in generate_signals(config) {
-        _ = effect_handler.send_message(signal.try_into()?).await;
+    // loop through each step
+
+    for step in steps {
+        // create batches if specified
+        let batches = step.get_batches_to_generate() as usize;
+        for _ in 0..batches {
+            let signal = match step.get_config() {
+                SignalConfig::Metric(config) => OTLPData::Metrics(config.get_signal()),
+                SignalConfig::Log(config) => OTLPData::Logs(config.get_signal()),
+                SignalConfig::Span(config) => OTLPData::Traces(config.get_signal()),
+            };
+            _ = effect_handler.send_message(signal.try_into()?).await;
+            // if there is a delay set between batches sleep for that amount before created the next signal in the batch
+            sleep(Duration::from_millis(step.get_delay_between_batches_ms())).await;
+        }
     }
 
     Ok(())
 }
 
-impl TryFrom<OTLPSignal> for OtapPdata {
+impl TryFrom<OTLPData> for OtapPdata {
     type Error = Error<OtapPdata>;
 
-    fn try_from(value: OTLPSignal) -> Result<Self, Self::Error> {
+    fn try_from(value: OTLPData) -> Result<Self, Self::Error> {
         let map_error = |e: EncodeError| {
             Error::from(pdata::error::Error::ConversionError {
                 error: format!("error encoding protobuf: {e}"),
@@ -128,17 +142,22 @@ impl TryFrom<OTLPSignal> for OtapPdata {
         };
         let mut bytes = vec![];
         Ok(match value {
-            OTLPSignal::Logs(logs_data) => {
+            OTLPData::Logs(logs_data) => {
                 logs_data.encode(&mut bytes).map_err(map_error)?;
                 OtlpProtoBytes::ExportLogsRequest(bytes).into()
             }
-            OTLPSignal::Metrics(metrics_data) => {
+            OTLPData::Metrics(metrics_data) => {
                 metrics_data.encode(&mut bytes).map_err(map_error)?;
                 OtlpProtoBytes::ExportMetricsRequest(bytes).into()
             }
-            OTLPSignal::Traces(trace_data) => {
+            OTLPData::Traces(trace_data) => {
                 trace_data.encode(&mut bytes).map_err(map_error)?;
                 OtlpProtoBytes::ExportTracesRequest(bytes).into()
+            }
+            _ => {
+                return Err(Error::from(pdata::error::Error::ConversionError {
+                    error: "unsupported signal type".into(),
+                }));
             }
         })
     }
@@ -151,67 +170,43 @@ mod tests {
     use otap_df_config::node::NodeUserConfig;
     use otap_df_engine::receiver::ReceiverWrapper;
     use otap_df_engine::testing::receiver::{NotSendValidateContext, TestContext, TestRuntime};
+    use otap_df_otlp::ExportTraceServiceRequest;
     use otap_df_otlp::fake_signal_receiver::config::{
-        Config, Load, OTLPSignal, ScenarioStep, SignalType,
+        Config, LogConfig, MetricConfig, MetricType, ScenarioStep, SignalConfig, SpanConfig,
     };
-    use otel_arrow_rust::proto::opentelemetry::{
-        logs::v1::LogsData,
-        metrics::v1::{Metric, MetricsData, metric::Data},
-        trace::v1::TracesData,
-    };
-    use std::fs;
+    use otap_df_otlp::proto::opentelemetry::collector::logs::v1::ExportLogsServiceRequest;
+    use otap_df_otlp::proto::opentelemetry::collector::metrics::v1::ExportMetricsServiceRequest;
+    use otap_df_otlp::proto::opentelemetry::metrics::v1::metric::Data;
     use std::future::Future;
     use std::pin::Pin;
     use std::rc::Rc;
     use tokio::time::{Duration, sleep, timeout};
-    use weaver_forge::registry::ResolvedRegistry;
 
     const RESOURCE_COUNT: usize = 1;
+    const SPAN_COUNT: usize = 1;
+    const METRIC_COUNT: usize = 1;
+    const LOG_COUNT: usize = 1;
+    const DATAPOINT_COUNT: usize = 1;
+    const ATTRIBUTE_COUNT: usize = 1;
+    const EVENT_COUNT: usize = 1;
+    const LINK_COUNT: usize = 1;
     const SCOPE_COUNT: usize = 1;
     const BATCH_COUNT: u64 = 1;
     const DELAY: u64 = 0;
-    const RESOLVED_REGISTRY_FILE: &str = "../otlp/src/fake_signal_receiver/resolved_registry.yaml";
 
-    // metric signal based on registry we should check matches
-    const METRIC_NAME: &str = "system.network.dropped";
-    const METRIC_DESC: &str =
-        "Count of packets that are dropped or discarded even though there was no error.";
-    const METRIC_DATAPOINT_ATTR: [&str; 2] = ["network.io.direction", "network.interface.name"];
-    const METRIC_UNIT: &str = "{packet}";
-
-    // span signal based on registry we should check matches
-    const SPAN_NAME: &str = "span.rpc.client";
-    const SPAN_ATTR: [&str; 9] = [
-        "rpc.method",
-        "rpc.service",
-        "network.peer.address",
-        "network.transport",
-        "network.type",
-        "rpc.system",
-        "network.peer.port",
-        "server.address",
-        "server.port",
-    ];
-    const SPAN_EVENTS: [&str; 1] = ["rpc.message"];
-
-    // log signal based on registry we should check matches
-    const LOG_NAME: &str = "session.end";
-    const LOG_ATTR: [&str; 1] = ["session.id"];
-
-    impl From<OtapPdata> for OTLPSignal {
+    impl From<OtapPdata> for OTLPData {
         fn from(value: OtapPdata) -> Self {
-            // TODO no unwrap?
             let otlp_bytes: OtlpProtoBytes =
                 value.try_into().expect("can convert signal to otlp bytes");
             match otlp_bytes {
-                OtlpProtoBytes::ExportLogsRequest(bytes) => {
-                    OTLPSignal::Logs(LogsData::decode(bytes.as_ref()).expect("can decode bytes"))
-                }
-                OtlpProtoBytes::ExportMetricsRequest(bytes) => OTLPSignal::Metrics(
-                    MetricsData::decode(bytes.as_ref()).expect("can decode bytes"),
+                OtlpProtoBytes::ExportLogsRequest(bytes) => Self::Logs(
+                    ExportLogsServiceRequest::decode(bytes.as_ref()).expect("can decode bytes"),
                 ),
-                OtlpProtoBytes::ExportTracesRequest(bytes) => OTLPSignal::Traces(
-                    TracesData::decode(bytes.as_ref()).expect("can decode bytes"),
+                OtlpProtoBytes::ExportMetricsRequest(bytes) => Self::Metrics(
+                    ExportMetricsServiceRequest::decode(bytes.as_ref()).expect("can decode bytes"),
+                ),
+                OtlpProtoBytes::ExportTracesRequest(bytes) => Self::Traces(
+                    ExportTraceServiceRequest::decode(bytes.as_ref()).expect("can decode bytes"),
                 ),
             }
         }
@@ -240,17 +235,17 @@ mod tests {
                 // check that messages have been sent through the effect_handler
 
                 // read from the effect handler
-                let metric_received = timeout(Duration::from_secs(3), ctx.recv())
+                let metric_received: OTLPData = timeout(Duration::from_secs(3), ctx.recv())
                     .await
                     .expect("Timed out waiting for message")
                     .expect("No message received")
                     .into();
-                let trace_received = timeout(Duration::from_secs(3), ctx.recv())
+                let trace_received: OTLPData = timeout(Duration::from_secs(3), ctx.recv())
                     .await
                     .expect("Timed out waiting for message")
                     .expect("No message received")
                     .into();
-                let log_received = timeout(Duration::from_secs(3), ctx.recv())
+                let log_received: OTLPData = timeout(Duration::from_secs(3), ctx.recv())
                     .await
                     .expect("Timed out waiting for message")
                     .expect("No message received")
@@ -258,102 +253,74 @@ mod tests {
 
                 // Assert that the message received is what the test client sent.
                 match metric_received {
-                    OTLPSignal::Metrics(metric) => {
+                    OTLPData::Metrics(metric) => {
                         // loop and check count
-                        let mut metric_seen = false;
                         let resource_count = metric.resource_metrics.len();
                         assert!(resource_count == RESOURCE_COUNT);
                         for resource in metric.resource_metrics.iter() {
                             let scope_count = resource.scope_metrics.len();
                             assert!(scope_count == SCOPE_COUNT);
                             for scope in resource.scope_metrics.iter() {
-                                for metric in scope.metrics.iter() {
-                                    // check for metric and see if the signal fields match what is defined in the registry
-                                    if &metric.name == METRIC_NAME {
-                                        metric_seen = true;
-                                        assert!(metric.description == METRIC_DESC.to_string());
-                                        assert!(metric.unit == METRIC_UNIT.to_string());
-                                        match metric.data.as_ref().expect("metric has no data") {
-                                            Data::Sum(sum) => {
-                                                assert!(sum.is_monotonic);
-                                                for datapoints in sum.data_points.iter() {
-                                                    let keys: Vec<&str> = datapoints
-                                                        .attributes
-                                                        .iter()
-                                                        .map(|attribute| attribute.key.as_str())
-                                                        .collect();
-                                                    assert!(keys == METRIC_DATAPOINT_ATTR.to_vec());
-                                                }
+                                let metric_count = scope.metrics.len();
+                                assert!(metric_count == METRIC_COUNT);
+                                for metric_data in scope.metrics.iter() {
+                                    if let Some(data) = &metric_data.data {
+                                        if let Data::Gauge(gauge) = data {
+                                            let datapoint_count = gauge.data_points.len();
+                                            assert!(datapoint_count == DATAPOINT_COUNT);
+                                            for datapoint in gauge.data_points.iter() {
+                                                let attribute_count = datapoint.attributes.len();
+                                                assert!(attribute_count == ATTRIBUTE_COUNT);
                                             }
-                                            _ => assert!(false),
+                                        } else {
+                                            unreachable!("Wrong MetricType received");
                                         }
+                                    } else {
+                                        unreachable!("Option should not be None");
                                     }
                                 }
                             }
                         }
-                        assert!(metric_seen);
                     }
                     _ => unreachable!("Signal should have been a Metric type"),
                 }
 
                 match trace_received {
-                    OTLPSignal::Traces(span) => {
-                        let mut span_seen = false;
+                    OTLPData::Traces(span) => {
                         let resource_count = span.resource_spans.len();
                         assert!(resource_count == RESOURCE_COUNT);
                         for resource in span.resource_spans.iter() {
                             let scope_count = resource.scope_spans.len();
                             assert!(scope_count == SCOPE_COUNT);
                             for scope in resource.scope_spans.iter() {
-                                for span in scope.spans.iter() {
-                                    // check for span and see if the signal fields match what is defined in the registry
-                                    if &span.name == SPAN_NAME {
-                                        span_seen = true;
-                                        let keys: Vec<&str> = span
-                                            .attributes
-                                            .iter()
-                                            .map(|attribute| attribute.key.as_str())
-                                            .collect();
-                                        assert!(keys == SPAN_ATTR);
-                                        let events: Vec<&str> = span
-                                            .events
-                                            .iter()
-                                            .map(|event| event.name.as_str())
-                                            .collect();
-                                        assert!(events == SPAN_EVENTS.to_vec())
-                                    }
+                                let span_count = scope.spans.len();
+                                assert!(span_count == SPAN_COUNT);
+                                for span_data in scope.spans.iter() {
+                                    let event_count = span_data.events.len();
+                                    let link_count = span_data.links.len();
+                                    let attribute_count = span_data.attributes.len();
+                                    assert!(link_count == LINK_COUNT);
+                                    assert!(event_count == EVENT_COUNT);
+                                    assert!(attribute_count == ATTRIBUTE_COUNT);
                                 }
                             }
                         }
-                        assert!(span_seen);
                     }
                     _ => unreachable!("Signal should have been a Span type"),
                 }
 
                 match log_received {
-                    OTLPSignal::Logs(log) => {
-                        let mut log_seen = false;
+                    OTLPData::Logs(log) => {
                         let resource_count = log.resource_logs.len();
                         assert!(resource_count == RESOURCE_COUNT);
                         for resource in log.resource_logs.iter() {
                             let scope_count = resource.scope_logs.len();
                             assert!(scope_count == SCOPE_COUNT);
                             for scope in resource.scope_logs.iter() {
-                                for log_record in scope.log_records.iter() {
-                                    // check for log and see if the signal fields match what is defined in the registry
-                                    if &log_record.event_name == LOG_NAME {
-                                        log_seen = true;
-                                        let keys: Vec<&str> = log_record
-                                            .attributes
-                                            .iter()
-                                            .map(|attribute| attribute.key.as_str())
-                                            .collect();
-                                        assert!(keys == LOG_ATTR.to_vec());
-                                    }
-                                }
+                                let log_count = scope.log_records.len();
+                                assert!(log_count == LOG_COUNT);
                             }
                         }
-                        assert!(log_seen);
                     }
                     _ => unreachable!("Signal should have been a Log type"),
                 }
@@ -362,39 +329,51 @@ mod tests {
     }
 
     #[test]
-    fn test_fake_signal_generator() {
+    fn test_fake_signal_receiver() {
         let test_runtime = TestRuntime::new();
 
-        let registry_yaml =
-            fs::read_to_string(RESOLVED_REGISTRY_FILE).expect("unable to read registry file");
-        let registry: ResolvedRegistry = serde_yaml::from_str(registry_yaml.as_ref()).unwrap();
-
         let mut steps = vec![];
+        let metric_config = MetricConfig::new(
+            RESOURCE_COUNT,
+            SCOPE_COUNT,
+            METRIC_COUNT,
+            DATAPOINT_COUNT,
+            MetricType::Gauge,
+            ATTRIBUTE_COUNT,
+        );
+        let trace_config = SpanConfig::new(
+            RESOURCE_COUNT,
+            SCOPE_COUNT,
+            SPAN_COUNT,
+            EVENT_COUNT,
+            LINK_COUNT,
+            ATTRIBUTE_COUNT,
+        );
 
-        let load = Load::new(RESOURCE_COUNT, SCOPE_COUNT);
+        let log_config = LogConfig::new(RESOURCE_COUNT, SCOPE_COUNT, LOG_COUNT, ATTRIBUTE_COUNT);
 
         steps.push(ScenarioStep::new(
-            SignalType::Metrics(load.clone()),
+            SignalConfig::Metric(metric_config),
             BATCH_COUNT,
             DELAY,
         ));
 
         steps.push(ScenarioStep::new(
-            SignalType::Traces(load.clone()),
+            SignalConfig::Span(trace_config),
             BATCH_COUNT,
             DELAY,
         ));
         steps.push(ScenarioStep::new(
-            SignalType::Logs(load.clone()),
+            SignalConfig::Log(log_config),
             BATCH_COUNT,
             DELAY,
         ));
-        let config = Config::new(steps, registry);
+        let config = Config::new(steps);
 
+        // create our receiver
         let node_config = Rc::new(NodeUserConfig::new_receiver_config(
             OTAP_FAKE_DATA_GENERATOR_URN,
         ));
-        // create our receiver
         let receiver = ReceiverWrapper::local(
             FakeGeneratorReceiver::new(config),
             node_config,
