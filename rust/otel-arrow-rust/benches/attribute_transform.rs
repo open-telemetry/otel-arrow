194fa71d88d777667759888bb1c49161db38ee4e
albertlockett <a.lockett@f5.com>
2025-08-08 12:51:42 -0300

    feat: add transform function to rename attribute keys (#893)
    Usage:
    
    ```rs
    let mut to_rename = BTreeMap::new();
    to_rename.insert("my_old_key", "new_key");
    to_rename.insert("my_old_key_2, "renamed_key_2");
    
    let mut otap_record: OtapArrowRecord = otap_pdata.try_into()?;
    let log_attrs = otap_record.get(ArrowPayloadType::LogAttrs).unwrap();
    otap_record.set(ArrowPayloadType::LogAttrs, rename_attributes(&log_attrs, &to_rename)?);
    ```
    
    The function tries to be pretty well optimized for the "happy path" use
    case, where we'd just be replacing a few dictionary string values. It
    does this by trying do copy values from the source array as few times as
    possible. However, it also tries to be general enough to handle the
    (hopefully unusual) use case that the attribute key column is not
    dictionary encoded and not sorted by key.
    
    Benchmark results:
    ```
    rename_attribute/single_replace/128
                            time:   [552.67 ns 555.38 ns 558.35 ns]
    
    rename_attribute/block_replace/128
                            time:   [787.24 ns 790.96 ns 794.66 ns]
    
    rename_attribute/many_non_contiguous_replace/128
                            time:   [824.48 ns 827.82 ns 831.17 ns]
    
    rename_attribute/single_replace/1536
                            time:   [3.5186 µs 3.5285 µs 3.5388 µs]
    
    rename_attribute/block_replace/1536
                            time:   [2.0743 µs 2.0856 µs 2.0962 µs]
    
    rename_attribute/many_non_contiguous_replace/1536
                            time:   [5.5591 µs 5.5763 µs 5.5954 µs]
    
    rename_attribute/single_replace/8092
                            time:   [8.7413 µs 8.7909 µs 8.8444 µs]
    
    rename_attribute/block_replace/8092
                            time:   [7.1287 µs 7.1537 µs 7.1793 µs]
    
    rename_attribute/many_non_contiguous_replace/8092
                            time:   [26.375 µs 26.445 µs 26.515 µs]
    ```

---
+++ rust/otel-arrow-rust/benches/attribute_transform.rs
@@ -0,0 +1,111 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//! Benchmarks for functions that transform attributes
+
+use arrow::array::{RecordBatch, StringBuilder};
+use arrow::datatypes::{DataType, Field, Schema};
+use criterion::{BatchSize, BenchmarkId, Criterion, criterion_group, criterion_main};
+use std::collections::BTreeMap;
+use std::hint::black_box;
+use std::sync::Arc;
+
+use otel_arrow_rust::otap::transform::rename_attributes;
+use otel_arrow_rust::schema::consts;
+
+fn generate_attribute_batch(num_rows: usize, key_gen: impl Fn(usize) -> String) -> RecordBatch {
+    let mut keys_arr = StringBuilder::new();
+    for i in 0..num_rows {
+        // let attr_key = format!("attr{}", i);
+        let attr_key = key_gen(i);
+        keys_arr.append_value(attr_key);
+    }
+
+    RecordBatch::try_new(
+        Arc::new(Schema::new(vec![Field::new(
+            consts::ATTRIBUTE_KEY,
+            DataType::Utf8,
+            false,
+        )])),
+        vec![Arc::new(keys_arr.finish())],
+    )
+    .expect("expect no error")
+}
+
+fn bench_attribute_rename(c: &mut Criterion) {
+    let mut group = c.benchmark_group("rename_attribute");
+    for size in [128, 1536, 8092] {
+        // this will generate a batch that replaces a single row, to simulate if the keys array
+        // was dictionary encoded
+        let single_replace_input = generate_attribute_batch(size, |i| format!("attr{i}"));
+        let _ = group.bench_with_input(
+            BenchmarkId::new("single_replace", size),
+            &single_replace_input,
+            |b, input| {
+                b.iter_batched(
+                    || input,
+                    |input| {
+                        let result = rename_attributes(
+                            input,
+                            &BTreeMap::from_iter([("attr100", "attr_100")]),
+                        )
+                        .expect("expect no errors");
+                        _ = black_box(result)
+                    },
+                    BatchSize::SmallInput,
+                )
+            },
+        );
+
+        // this will generate a batch that replaces a contiguous block of attributes, to simulate
+        // if attrs key was not dictionary encoded and the batch was sorted by key
+        let block_replace_input = generate_attribute_batch(size, |i| format!("attr{}", i / 20));
+        let _ = group.bench_with_input(
+            BenchmarkId::new("block_replace", size),
+            &block_replace_input,
+            |b, input| {
+                b.iter_batched(
+                    || input,
+                    |input| {
+                        let result =
+                            rename_attributes(input, &BTreeMap::from_iter([("attr3", "attr_3")]))
+                                .expect("expect no errors");
+                        _ = black_box(result)
+                    },
+                    BatchSize::SmallInput,
+                )
+            },
+        );
+
+        // this will generate a batch that replaces many non-contiguous rows, to simulate if the
+        // attrs key was not dictionary encoded and the batch was not sorted by key. This could
+        // happen when we encode OTAP from OTLP, where the attributes end up sorted by parent ID
+        let multi_non_contiguous_input =
+            generate_attribute_batch(size, |i| format!("attr{}", i % 20));
+        let _ = group.bench_with_input(
+            BenchmarkId::new("many_non_contiguous_replace", size),
+            &multi_non_contiguous_input,
+            |b, input| {
+                b.iter_batched(
+                    || input,
+                    |input| {
+                        let result =
+                            rename_attributes(input, &BTreeMap::from_iter([("attr3", "attr_3")]))
+                                .expect("expect no errors");
+                        _ = black_box(result)
+                    },
+                    BatchSize::SmallInput,
+                )
+            },
+        );
+    }
+
+    group.finish();
+}
+
+#[allow(missing_docs)]
+mod benches {
+    use super::*;
+    criterion_group!(benches, bench_attribute_rename);
+}
+criterion_main!(benches::benches);
