ac5a7d7134b479e25c9bae643a70f15cc5d7fb7f
Mikel Blanchard <mblanchard@macrosssoftware.com>
2025-08-13 13:51:17 -0700

    [query-engine] Refactor StringReplace into text group. (#935)
    ## Changes
    
    * Introduce a group for scalars which operate on text and move
    `ReplaceString` into that group

--- rust/experimental/query_engine/expressions/src/scalars/mod.rs
+++ rust/experimental/query_engine/expressions/src/scalars/mod.rs
@@ -4,6 +4,7 @@ pub(crate) mod parse_scalar_expression;
 pub(crate) mod scalar_expressions;
 pub(crate) mod statics;
 pub(crate) mod temporal_scalar_expression;
+pub(crate) mod text_scalar_expression;
 
 pub use convert_scalar_expression::*;
 pub use math_scalar_expression::*;
@@ -11,3 +12,4 @@ pub use parse_scalar_expression::*;
 pub use scalar_expressions::*;
 pub use statics::*;
 pub use temporal_scalar_expression::*;
+pub use text_scalar_expression::*;
