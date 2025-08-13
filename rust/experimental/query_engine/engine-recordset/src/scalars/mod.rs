24074e8aac5744c37589e587866d4100f6db0ea9
Mikel Blanchard <mblanchard@macrosssoftware.com>
2025-08-13 08:20:42 -0700

    [query-engine] Conversion scalar expression reorg (#930)
    ## Changes
    
    * Moves the implementation of the conversion scalar expressions into its
    own file in recordset engine (mirrors math, temporal, and parse groups)

--- rust/experimental/query_engine/engine-recordset/src/scalars/mod.rs
+++ rust/experimental/query_engine/engine-recordset/src/scalars/mod.rs
@@ -1,8 +1,10 @@
+pub(crate) mod convert_scalar_expressions;
 pub(crate) mod math_scalar_expressions;
 pub(crate) mod parse_scalar_expressions;
 pub(crate) mod scalar_expressions;
 pub(crate) mod temporal_scalar_expressions;
 
+pub use convert_scalar_expressions::*;
 pub use math_scalar_expressions::*;
 pub use parse_scalar_expressions::*;
 pub use scalar_expressions::*;
