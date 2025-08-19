6000b0a433bde8588cb4061d2f49fc0239988d90
Drew Relmas <drewrelmas@microsoft.com>
2025-08-19 16:29:34 +0000

    Add missing

--- rust/otel-arrow-rust/src/otlp/attributes/decoder.rs
+++ rust/otel-arrow-rust/src/otlp/attributes/decoder.rs
@@ -1,18 +1,6 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 // https://github.com/open-telemetry/otel-arrow/blob/985aa1500a012859cec44855e187eacf46eda7c8/pkg/otel/common/arrow/attributes.go#L40
 
 use crate::otlp::attributes::parent_id::ParentId;
