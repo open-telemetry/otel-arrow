fb3348c3382e31cff46ee71fae546832655abc51
Chris Hain <c.hain@f5.com>
2025-08-29 10:48:01 -0700

    add metrics to fake signal receiver

---
+++ rust/otap-dataflow/crates/otlp/src/fake_signal_receiver/metrics.rs
@@ -0,0 +1,22 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//! Metrics for the OTLP Fake Signal Receiver node.
+
+use otap_df_telemetry::instrument::Counter;
+use otap_df_telemetry_macros::metric_set;
+
+/// Pdata-oriented metrics for the OTAP PerfExporter.
+#[metric_set(name = "fake_data_generator.receiver.metrics")]
+#[derive(Debug, Default, Clone)]
+pub struct FakeSignalReceiverMetrics {
+    /// Number of logs generated.
+    #[metric(unit = "{log}")]
+    pub logs: Counter<u64>,
+    /// Number of spans generated.
+    #[metric(unit = "{span}")]
+    pub spans: Counter<u64>,
+    /// Number of metrics generated.
+    #[metric(unit = "{metric}")]
+    pub metrics: Counter<u64>,
+}
