07ad0aefe80533a153601da568e64acc8e068bb9
Laurent Quérel <l.querel@f5.com>
2025-09-04 08:58:48 -0700

    Endpoints to expose the current overall state of the pipelines and to stop pipelines (#1033)
    This PR provides additional visibility into the current state of the
    system and pipelines. It is now also possible to stop pipelines through
    a shutdown endpoint.
    
    List of admin endpoints added in this PR:
    - Get the status of the specified pipeline: GET
    /pipeline-groups/{pipeline_group_id}/pipelines/{pipeline_id}/status
    - Get the status of all the pipeline groups: GET /pipeline-groups/status
    - Shutdown all pipeline groups: POST /pipeline-groups/shutdown
    
    Heartbeat events are not yet implemented and will be added in a future
    PR.
    
    Background context: This PR is the first step of the implementation of
    the reconciler concept.
    A global observed state is now kept up to date by monitoring the changes
    made to/by the pipelines (and groups).
    In future PRs, a desired state will be submitted to a reconciler in
    order to update the current configuration/execution of the pipelines.
    The ultimate goal is to support live reconfigurations and provide full
    observability of the pipelines' state.

--- rust/otap-dataflow/crates/admin/src/lib.rs
+++ rust/otap-dataflow/crates/admin/src/lib.rs
@@ -4,6 +4,7 @@
 //! HTTP server for exposing admin endpoints.
 
 pub mod error;
+mod pipeline;
 mod pipeline_group;
 mod telemetry;
 
@@ -16,23 +17,32 @@ use tower::ServiceBuilder;
 use crate::error::Error;
 use otap_df_config::engine::HttpAdminSettings;
 use otap_df_engine::control::PipelineCtrlMsgSender;
+use otap_df_state::store::ObservedStateHandle;
 use otap_df_telemetry::registry::MetricsRegistryHandle;
 
 /// Shared state for the HTTP admin server.
 #[derive(Clone)]
 struct AppState {
+    /// The observed state store for querying the current state of the entire system.
+    observed_state_store: ObservedStateHandle,
+
+    /// The metrics registry for querying current metrics.
     metrics_registry: MetricsRegistryHandle,
+
+    /// The control message senders for controlling pipelines.
     ctrl_msg_senders: Vec<PipelineCtrlMsgSender>,
 }
 
 /// Run the admin HTTP server until shutdown is requested.
 pub async fn run(
     config: HttpAdminSettings,
+    observed_store: ObservedStateHandle,
     ctrl_msg_senders: Vec<PipelineCtrlMsgSender>,
     metrics_registry: MetricsRegistryHandle,
     cancel: CancellationToken,
 ) -> Result<(), Error> {
     let app_state = AppState {
+        observed_state_store: observed_store,
         metrics_registry,
         ctrl_msg_senders,
     };
@@ -40,6 +50,7 @@ pub async fn run(
     let app = Router::new()
         .merge(telemetry::routes())
         .merge(pipeline_group::routes())
+        .merge(pipeline::routes())
         .layer(ServiceBuilder::new())
         .with_state(app_state);
 
