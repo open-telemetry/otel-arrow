af78945d30864e86b62da33aecf996aae9f1716d
c1ly <c.ly@f5.com>
2025-09-04 16:09:55 -0700

    defined the internal metrics to be collected from the debug processor

---
+++ rust/otap-dataflow/crates/otap/src/debug_processor/metrics.rs
@@ -0,0 +1,34 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//! Metrics for the OTAP PerfExporter node.
+
+use otap_df_telemetry::instrument::Counter;
+use otap_df_telemetry_macros::metric_set;
+
+/// Pdata-oriented metrics for the OTAP DebugProcessor
+#[metric_set(name = "debug.processor.pdata.metrics")]
+#[derive(Debug, Default, Clone)]
+pub struct DebugPdataMetrics {
+    /// Number of logs seen
+    #[metric(unit = "{log}")]
+    pub logs: Counter<u64>,
+    /// Number of events (structured logs) seen
+    #[metric(unit = "{event}")]
+    pub events: Counter<u64>,
+    /// Number of spans seen
+    #[metric(unit = "{span}")]
+    pub spans: Counter<u64>,
+    /// number of span links seen
+    #[metric(unit = "{link}")]
+    pub span_links: Counter<u64>,
+    /// number of span events (structured logs) seen
+    #[metric(unit = "{event}")]
+    pub span_events: Counter<u64>,
+    /// Number of metrics seen
+    #[metric(unit = "{metric}")]
+    pub metrics: Counter<u64>,
+    /// number of metric datapoints seen
+    #[metric(unit = "{datapoint}")]
+    pub metric_datapoints: Counter<u64>,
+}
