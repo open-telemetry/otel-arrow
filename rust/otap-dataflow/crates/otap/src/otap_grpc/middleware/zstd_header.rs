59a165d23bead42112c8f8fbf3ed5ec02a2fd5f3
albertlockett <a.lockett@f5.com>
2025-09-08 19:09:43 -0400

    PR feedback

--- rust/otap-dataflow/crates/otap/src/otap_grpc/middleware/zstd_header.rs
+++ rust/otap-dataflow/crates/otap/src/otap_grpc/middleware/zstd_header.rs
@@ -4,11 +4,12 @@
 //! Middlewares for adapting gRPC encoding request header
 
 use async_trait::async_trait;
-use http::{Request, Response};
+use http::{HeaderName, HeaderValue, Request, Response};
 use tonic::body::Body;
 use tonic_middleware::{Middleware, ServiceBound};
 
-const ENCODING_HEADER: &str = "grpc-encoding";
+static ENCODING_HEADER: HeaderName = HeaderName::from_static("grpc-encoding");
+static ZSTD_HEADER_VALUE: HeaderValue = HeaderValue::from_static("zstd");
 
 /// Tonic middleware implementation that will replace the `grpc-encoding` header that the golang
 /// exporter produces (which has the format zstdarrow[0-9]) with the value that tonic expects,
@@ -28,13 +29,10 @@ where
         mut service: S,
     ) -> Result<Response<Body>, S::Error> {
         let headers = req.headers_mut();
-        if let Some(header_val) = headers.get(ENCODING_HEADER) {
+        if let Some(header_val) = headers.get(&ENCODING_HEADER) {
             let header_bytes = header_val.as_bytes();
             if header_bytes.starts_with(b"zstdarrow") {
-                _ = headers.insert(
-                    ENCODING_HEADER,
-                    "zstd".parse().expect("can parse to header value"),
-                );
+                _ = headers.insert(&ENCODING_HEADER, ZSTD_HEADER_VALUE.clone());
             }
         }
 
