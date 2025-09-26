a6eaf1244160cf2466d406974db90f1432cd4922
Utkarsh Umesan Pillai <66651184+utpilla@users.noreply.github.com>
2025-09-26 23:25:29 +0000

    Add support for escape characters in CEF parser

--- rust/otap-dataflow/crates/otap/src/syslog_cef_receiver/parser/parsed_message.rs
+++ rust/otap-dataflow/crates/otap/src/syslog_cef_receiver/parser/parsed_message.rs
@@ -253,7 +253,7 @@ impl ParsedSyslogMessage<'_> {
                 log_attributes_arrow_records
                     .append_str(std::str::from_utf8(msg.severity).unwrap_or_default());
 
-                for (key, value) in msg.parse_extensions() {
+                while let Some((key, value)) = msg.parse_extensions().next_extension() {
                     log_attributes_arrow_records
                         .append_key(std::str::from_utf8(key).unwrap_or_default());
                     log_attributes_arrow_records
