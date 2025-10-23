cc55f8387dc64019ca2cec6c5c0cfad544b4146a
albertlockett <a.lockett@f5.com>
2025-10-23 13:53:46 -0400

    else-if supported plus fixed tests plus better error handling

--- rust/experimental/query_engine/engine-columnar/src/error.rs
+++ rust/experimental/query_engine/engine-columnar/src/error.rs
@@ -1,7 +1,23 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+use datafusion::error::DataFusionError;
+
 pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(thiserror::Error, Debug)]
-pub enum Error {}
+pub enum Error {
+    /// Error the engine will produce if it receives a pipeline expression that is not valid
+    #[error("Invalid pipeline error: {reason}")]
+    InvalidPipelineError { reason: String },
+
+    #[error("Engine received invalid record batch: {reason}")]
+    InvalidBatchError { reason: String },
+
+    /// Error for syntax that should be valid but is not yet supported by this engine
+    #[error("Operation not yet supported by columnar engine: {message}")]
+    NotYetSupportedError { message: String },
+
+    #[error("Error occurred in DataFusion: {0}")]
+    DataFusion(#[from] DataFusionError),
+}
