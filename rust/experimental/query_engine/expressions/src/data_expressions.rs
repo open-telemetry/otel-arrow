f23cd4bd7a69def7d4f974f4a5f57ab2f715152e
albertlockett <a.lockett@f5.com>
2025-10-23 10:03:03 -0400

    added definition for conditional data statement

--- rust/experimental/query_engine/expressions/src/data_expressions.rs
+++ rust/experimental/query_engine/expressions/src/data_expressions.rs
@@ -13,6 +13,8 @@ pub enum DataExpression {
 
     /// Transform data expression.
     Transform(TransformExpression),
+
+    Conditional(ConditionalDataExpression),
 }
 
 impl DataExpression {
@@ -24,6 +26,9 @@ impl DataExpression {
             DataExpression::Discard(d) => d.try_fold(scope),
             DataExpression::Summary(s) => s.try_fold(scope),
             DataExpression::Transform(t) => t.try_fold(scope),
+            DataExpression::Conditional(_) => {
+                todo!("try_fold conditional data expr")
+            }
         }
     }
 }
@@ -34,6 +39,7 @@ impl Expression for DataExpression {
             DataExpression::Discard(d) => d.get_query_location(),
             DataExpression::Summary(s) => s.get_query_location(),
             DataExpression::Transform(t) => t.get_query_location(),
+            DataExpression::Conditional(c) => c.get_query_location(),
         }
     }
 
@@ -42,6 +48,7 @@ impl Expression for DataExpression {
             DataExpression::Discard(_) => "DataExpression(Discard)",
             DataExpression::Summary(_) => "DataExpression(Summary)",
             DataExpression::Transform(_) => "DataExpression(Transform)",
+            DataExpression::Conditional(_) => "DataExpression(Conditional)",
         }
     }
 
@@ -50,6 +57,7 @@ impl Expression for DataExpression {
             DataExpression::Discard(d) => d.fmt_with_indent(f, indent),
             DataExpression::Summary(s) => s.fmt_with_indent(f, indent),
             DataExpression::Transform(t) => t.fmt_with_indent(f, indent),
+            DataExpression::Conditional(c) => c.fmt_with_indent(f, indent),
         }
     }
 }
@@ -114,6 +122,32 @@ impl Expression for DiscardDataExpression {
                 p.fmt_with_indent(f, format!("{indent}        ").as_str())?;
             }
         }
+        Ok(())
+    }
+}
+
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ConditionalDataExpression {
+    query_location: QueryLocation,
+    branches: Vec<(LogicalExpression, Vec<DataExpression>)>,
+    default_branch: Option<Vec<DataExpression>>,
+
+}
+
+
+impl Expression for ConditionalDataExpression {
+    fn get_query_location(&self) -> &QueryLocation {
+        &self.query_location
+    }
+
+    fn get_name(&self) -> &'static str {
+        "ConditionalDataExpression"
+    }
+
+    fn fmt_with_indent(&self, f: &mut std::fmt::Formatter<'_>, indent: &str) -> std::fmt::Result {
+        writeln!(f, "Conditional")?;
+        writeln!(f, "{indent}└── TODO ")?;
         Ok(())
     }
 }
