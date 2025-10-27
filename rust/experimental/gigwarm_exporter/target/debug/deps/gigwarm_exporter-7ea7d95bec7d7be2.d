9d9b24bc5e69e27c84b11ac6e4f02e62dd943b2b
Lalit Bhasin <lalit_fin@yahoo.com>
2025-10-27 15:00:34 -0700

    remove leftovers

--- rust/experimental/gigwarm_exporter/target/debug/deps/gigwarm_exporter-7ea7d95bec7d7be2.d
+++
@@ -1,9 +0,0 @@
-/Users/lalitb/work/obs/otel/rust-collector-2/otel-arrow-2/otel-arrow/rust/experimental/gigwarm_exporter/target/debug/deps/gigwarm_exporter-7ea7d95bec7d7be2.d: src/lib.rs
-
-/Users/lalitb/work/obs/otel/rust-collector-2/otel-arrow-2/otel-arrow/rust/experimental/gigwarm_exporter/target/debug/deps/libgigwarm_exporter-7ea7d95bec7d7be2.rlib: src/lib.rs
-
-/Users/lalitb/work/obs/otel/rust-collector-2/otel-arrow-2/otel-arrow/rust/experimental/gigwarm_exporter/target/debug/deps/libgigwarm_exporter-7ea7d95bec7d7be2.rmeta: src/lib.rs
-
-src/lib.rs:
-
-# env-dep:CARGO_PKG_VERSION=0.0.0
