1a5a1fd168362b74756e337e83e60adb71936857
albertlockett <a.lockett@f5.com>
2025-11-05 08:25:10 -0500

    initial implementation of metrics OTLP bytes -> OTAP

--- rust/otap-dataflow/crates/pdata/src/views/otlp/bytes/metrics.rs
+++ rust/otap-dataflow/crates/pdata/src/views/otlp/bytes/metrics.rs
@@ -10,15 +10,21 @@ use std::num::NonZeroUsize;
 use otel_arrow_rust::proto::consts::field_num::metrics::{
     METRIC_DESCRIPTION, METRIC_EXPONENTIAL_HISTOGRAM, METRIC_GAUGE, METRIC_HISTOGRAM,
     METRIC_METADATA, METRIC_NAME, METRIC_SUM, METRIC_SUMMARY, METRIC_UNIT,
-    RESOURCE_METRICS_RESOURCE, RESOURCE_METRICS_SCHEMA_URL, RESOURCE_METRICS_SCOPE_METRICS,
-    SCOPE_METRICS_METRICS, SCOPE_METRICS_SCHEMA_URL, SCOPE_METRICS_SCOPE,
+    METRICS_DATA_RESOURCE_METRICS, RESOURCE_METRICS_RESOURCE, RESOURCE_METRICS_SCHEMA_URL,
+    RESOURCE_METRICS_SCOPE_METRICS, SCOPE_METRICS_METRICS, SCOPE_METRICS_SCHEMA_URL,
+    SCOPE_METRICS_SCOPE,
 };
 use otel_arrow_rust::proto::consts::wire_types;
 
-use crate::views::metrics::{MetricView, ResourceMetricsView, ScopeMetricsView};
+use crate::views::common::Str;
+use crate::views::metrics::{MetricView, MetricsView, ResourceMetricsView, ScopeMetricsView};
+use crate::views::otlp::bytes::common::{KeyValueIter, RawInstrumentationScope, RawKeyValue};
 use crate::views::otlp::bytes::decode::{
-    FieldRanges, ProtoBytesParser, from_option_nonzero_range_to_primitive, to_nonzero_range,
+    FieldRanges, ProtoBytesParser, RepeatedFieldProtoBytesParser,
+    from_option_nonzero_range_to_primitive, read_len_delim, read_varint, to_nonzero_range,
 };
+use crate::views::otlp::bytes::resource::RawResource;
+use crate::views::otlp::proto::metrics::ObjData;
 
 /// Implementation of [`MetricView`] backed by protobuf serialized `MetricsData` message
 pub struct RawMetricsData<'a> {
@@ -86,7 +92,7 @@ impl FieldRanges for ResourceMetricsFieldRanges {
     }
 }
 
-/// Implementation of [`MetricView`] backed by protobuf serialized `ResourceMetrics` message
+/// Implementation of [`ScopeMetricsView`] backed by protobuf serialized `ScopeMetrics` message
 pub struct RawScopeMetrics<'a> {
     byte_parser: ProtoBytesParser<'a, ScopeMetricsFieldRanges>,
 }
@@ -139,6 +145,7 @@ impl FieldRanges for ScopeMetricsFieldRanges {
     }
 }
 
+/// Implementation of [`MetricView`] backed by protobuf serialized `Metric` message
 pub struct RawMetric<'a> {
     byte_parser: ProtoBytesParser<'a, MetricFieldRanges>,
 }
@@ -180,5 +187,248 @@ impl FieldRanges for MetricFieldRanges {
         from_option_nonzero_range_to_primitive(range)
     }
 
-    fn set_field_range(&self, field_num: u64, wire_type: u64, start: usize, end: usize) {}
+    fn set_field_range(&self, field_num: u64, wire_type: u64, start: usize, end: usize) {
+        if wire_type == wire_types::LEN {
+            let range = match to_nonzero_range(start, end) {
+                Some(range) => range,
+                None => return,
+            };
+
+            match field_num {
+                METRIC_NAME => self.name.set(Some(range)),
+                METRIC_DESCRIPTION => self.description.set(Some(range)),
+                METRIC_UNIT => self.unit.set(Some(range)),
+                METRIC_GAUGE | METRIC_SUM | METRIC_HISTOGRAM | METRIC_EXPONENTIAL_HISTOGRAM => {
+                    self.data.set(Some(range))
+                }
+                METRIC_METADATA => {
+                    if self.first_metadata.get().is_none() {
+                        self.first_metadata.set(Some(range))
+                    }
+                }
+                _ => { /* ignore */ }
+            }
+        }
+    }
+}
+
+// TODO field ranges etc. for additional metrics messages
+
+/* ───────────────────────────── ADAPTER ITERATORS ─────────────────────── */
+
+/// Iterator of ResourceMetrics - produces implementation of [`ResourceMetricsView`] from byte
+/// array containing serialized `MetricsData` message
+pub struct ResourceMetricsIter<'a> {
+    buf: &'a [u8],
+    pos: usize,
+}
+
+impl<'a> Iterator for ResourceMetricsIter<'a> {
+    type Item = RawResourceMetrics<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.pos < self.buf.len() {
+            let (tag, next_pos) = read_varint(self.buf, self.pos)?;
+            self.pos = next_pos;
+            let field = tag >> 3;
+            let wire_type = tag & 7;
+            if field == METRICS_DATA_RESOURCE_METRICS && wire_type == wire_types::LEN {
+                let (slice, next_pos) = read_len_delim(self.buf, self.pos)?;
+                self.pos = next_pos;
+                return Some(RawResourceMetrics {
+                    byte_parser: ProtoBytesParser::new(slice),
+                });
+            }
+        }
+
+        None
+    }
+}
+
+/// Iterator of ScopeMetrics - produces an implementation of [`ScopeMetricsView`] from byte array
+/// containing a serialized ResourceMetrics message
+pub struct ScopeMetricsIter<'a> {
+    byte_parser: RepeatedFieldProtoBytesParser<'a, ResourceMetricsFieldRanges>,
+}
+
+impl<'a> Iterator for ScopeMetricsIter<'a> {
+    type Item = RawScopeMetrics<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let slice = self.byte_parser.next()?;
+
+        Some(RawScopeMetrics {
+            byte_parser: ProtoBytesParser::new(slice),
+        })
+    }
+}
+
+/// Iterator of Metric - produces implementation of [`MetricView`] from byte array containing a
+/// serialized ScopeMetrics object
+pub struct MetricIter<'a> {
+    byte_parser: RepeatedFieldProtoBytesParser<'a, ScopeMetricsFieldRanges>,
+}
+
+impl<'a> Iterator for MetricIter<'a> {
+    type Item = RawMetric<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let slice = self.byte_parser.next()?;
+
+        Some(RawMetric {
+            byte_parser: ProtoBytesParser::new(slice),
+        })
+    }
+}
+
+/* ───────────────────────────── TRAIT IMPLEMENTATIONS ─────────────────── */
+
+impl MetricsView for RawMetricsData<'_> {
+    type ResourceMetrics<'res>
+        = RawResourceMetrics<'res>
+    where
+        Self: 'res;
+    type ResourceMetricsIter<'res>
+        = ResourceMetricsIter<'res>
+    where
+        Self: 'res;
+
+    #[inline]
+    fn resources(&self) -> Self::ResourceMetricsIter<'_> {
+        ResourceMetricsIter {
+            buf: self.buf,
+            pos: 0,
+        }
+    }
+}
+
+impl ResourceMetricsView for RawResourceMetrics<'_> {
+    type Resource<'res>
+        = RawResource<'res>
+    where
+        Self: 'res;
+    type ScopeMetrics<'scp>
+        = RawScopeMetrics<'scp>
+    where
+        Self: 'scp;
+    type ScopesIter<'scp>
+        = ScopeMetricsIter<'scp>
+    where
+        Self: 'scp;
+
+    #[inline]
+    fn resource(&self) -> Option<Self::Resource<'_>> {
+        let slice = self
+            .byte_parser
+            .advance_to_find_field(RESOURCE_METRICS_RESOURCE)?;
+        Some(RawResource::new(ProtoBytesParser::new(slice)))
+    }
+
+    #[inline]
+    fn schema_url(&self) -> Str<'_> {
+        self.byte_parser
+            .advance_to_find_field(RESOURCE_METRICS_SCHEMA_URL)
+            .unwrap_or_default()
+    }
+
+    #[inline]
+    fn scopes(&self) -> Self::ScopesIter<'_> {
+        ScopeMetricsIter {
+            byte_parser: RepeatedFieldProtoBytesParser::from_byte_parser(
+                &self.byte_parser,
+                RESOURCE_METRICS_SCOPE_METRICS,
+                wire_types::LEN,
+            ),
+        }
+    }
+}
+
+impl ScopeMetricsView for RawScopeMetrics<'_> {
+    type Metric<'met>
+        = RawMetric<'met>
+    where
+        Self: 'met;
+    type MetricIter<'met>
+        = MetricIter<'met>
+    where
+        Self: 'met;
+    type Scope<'scp>
+        = RawInstrumentationScope<'scp>
+    where
+        Self: 'scp;
+
+    #[inline]
+    fn schema_url(&self) -> Str<'_> {
+        self.byte_parser
+            .advance_to_find_field(SCOPE_METRICS_SCHEMA_URL)
+            .unwrap_or_default()
+    }
+
+    #[inline]
+    fn scope(&self) -> Option<Self::Scope<'_>> {
+        let slice = self
+            .byte_parser
+            .advance_to_find_field(SCOPE_METRICS_SCOPE)?;
+        Some(RawInstrumentationScope::new(ProtoBytesParser::new(slice)))
+    }
+
+    #[inline]
+    fn metrics(&self) -> Self::MetricIter<'_> {
+        MetricIter {
+            byte_parser: RepeatedFieldProtoBytesParser::from_byte_parser(
+                &self.byte_parser,
+                SCOPE_METRICS_METRICS,
+                wire_types::LEN,
+            ),
+        }
+    }
+}
+
+impl MetricView for RawMetric<'_> {
+    type Attribute<'att>
+        = RawKeyValue<'att>
+    where
+        Self: 'att;
+    type AttributeIter<'att>
+        = KeyValueIter<'att, MetricFieldRanges>
+    where
+        Self: 'att;
+
+    // TODO - this is a placeholder so the code compiles. We don't have any other implementations
+    // of DataView, so just referencing the proto structs version for now.
+    type Data<'dat>
+        = ObjData<'dat>
+    where
+        Self: 'dat;
+
+    fn name(&self) -> Str<'_> {
+        self.byte_parser
+            .advance_to_find_field(METRIC_NAME)
+            .unwrap_or_default()
+    }
+
+    fn description(&self) -> Str<'_> {
+        self.byte_parser
+            .advance_to_find_field(METRIC_DESCRIPTION)
+            .unwrap_or_default()
+    }
+
+    fn unit(&self) -> Str<'_> {
+        self.byte_parser
+            .advance_to_find_field(METRIC_UNIT)
+            .unwrap_or_default()
+    }
+
+    fn data(&self) -> Option<Self::Data<'_>> {
+        // TODO add implementation of metrics data
+        None
+    }
+
+    fn metadata(&self) -> Self::AttributeIter<'_> {
+        KeyValueIter::new(RepeatedFieldProtoBytesParser::from_byte_parser(
+            &self.byte_parser,
+            METRIC_METADATA,
+            wire_types::LEN,
+        ))
+    }
 }
