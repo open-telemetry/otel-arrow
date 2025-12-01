35380f80ba8530d1e95146ca237728da4f97dadd
albertlockett <a.lockett@f5.com>
2025-12-01 16:01:37 -0500

    Columnar query engine filter processor initial implementation (#1477)
    closes #1410 
    
    Adds implementation of `PipelineStage` that performs filtering called
    `FilterPipelineStage`.
    
    The implementation is a hybrid approach between using DataFusion and
    apache arrow compute functions. Each binary expression in the filter
    predicate is implemented as a type called `FilterExec`, which implements
    filtering by:
    - applies to the root `RecordBatch` DataFusion's
    [`PhysicalExpr`](https://docs.rs/datafusion-physical-expr/latest/datafusion_physical_expr/trait.PhysicalExpr.html)
    which returns a selection vector (a `BooleanArray` which has value
    `true` for rows that pass the filter).
    - invoking `AttributeFilterExec` which, applies `PhysicalExpr` to an
    attributes record batch, and returns a bitmap of which parent_ids are
    selected that I've called an "id mask" in the code. This is then
    combined with selection vector for the root using arrow compute
    functions.
    
    Boolean combinations of filter expressions (`and`, `or` and `not`) are
    planned and executed by a generic enum called `Composite<T>` which has
    capability to combine the selection vectors and id_masks for each
    `FilterExec` / `AttributeFilterExec` contained within it.
    
    This has some advantages over what was described in
    https://github.com/open-telemetry/otel-arrow/issues/1410 and/or just
    using a regular DataFusion `ExecutionPlan` to do the filtering:
    - It's easier to plan, especially because the attribute filtering is not
    driven by Joins and Distinct Unions
    - Each step in the filtering not need to materialize the entire arrow
    record batch -- only the selection vectors & id masks are required which
    can be easily combined using arrow compute functions.
    
    A basic `PipelinePlanner` is implemented that is able to produce
    `FilterPipelineStage` from the AST in
    `data_engine_expressions::PipelineExpression`.
    
    Many types of filters are already supported:
    ```kql
    // simple filtering by fields
    logs | where severity_text == "ERROR"
    
    // simple filtering by attributes
    logs | where attributes["x"] == "y"
    
    // comparing numbers
    logs | where attributes["z"] > 10 // also supports ">=", "<" & "<=" numeric comparisons
    
    // filtering by resource & scope
    logs | where instrumentation_scope.name == "otel-sdk"
    logs | where resource.schema_url = "opentelemetry.io/schema/v1"
    logs | where instrumentation_scope.attributes["x"] == "y"
    logs | where resource.attributes["x"] == "y"
    
    // putting the literal on the left
    logs | where "ERROR" == severity_text
    
    // logical combinations of any of the above using "and", "or" and "not"
    logs | where not(severity_text == "ERROR" or severity_text == "INFO") and event_name == "x" // etc.
    ```
    
    There are still many TODOs in the code which I will make issues to
    resolve. Notably, what's not supported/not working:
    - Empty batches (missing root `RecordBatch`)
    - Missing attribute `RecordBatch`s
    - Updating the plan when schemas changing
    - Comparing two fields (`logs | where severity_text == event_name` or
    `logs | where attributes["x"] == attributes["z"]`)
    - Comparing two literals (`logs | where 1 == 2`)
    - Filtering by integers that aren't type i64
    - Comparing text using "contains" or regex matches
    - Metrics & Traces are not supported
    - Any kind of arithmetic or function calls in the filter expressions
    
    However before stepping into the list above the next direction I'd like
    to take this is to:
    - Add benchmarks
    - Work on optimizing the query plan. Notably, I think there's places we
    could combine the logical expressions in `FilterPlan` and
    `AttributesFilterPlan` into a single `PhysicalExpr` which might give us
    better query performance.
    
    I realize this PR is quite long (although a lot of the volume it is
    tests). I'm happy to break it up if anyone reviewing would prefer.

---
+++ rust/experimental/query_engine/engine-columnar/src/consts.rs
@@ -0,0 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+pub(crate) const ATTRIBUTES_FIELD_NAME: &str = "attributes";
+pub(crate) const RESOURCES_FIELD_NAME: &str = "resource";
+pub(crate) const SCOPE_FIELD_NAME: &str = "instrumentation_scope";
