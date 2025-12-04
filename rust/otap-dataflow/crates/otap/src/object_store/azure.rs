29f07a06539cc9079df49496829535dd901eeb40
Jake Dern <jakedern@yahoo.com>
2025-12-04 14:19:40 -0800

    fix

--- rust/otap-dataflow/crates/otap/src/object_store/azure.rs
+++ rust/otap-dataflow/crates/otap/src/object_store/azure.rs
@@ -44,15 +44,14 @@ impl CredentialProvider for AzureTokenCredentialProvider {
     type Credential = AzureCredential;
 
     async fn get_credential(&self) -> object_store::Result<Arc<Self::Credential>> {
-        let maybe_new_token = dbg!(
-            self.token_cred
-                .get_token(&[&self.storage_scope], None)
-                .await
-                .map_err(|e| object_store::Error::Generic {
-                    store: "Azure",
-                    source: Box::new(e),
-                })
-        )?;
+        let maybe_new_token = self
+            .token_cred
+            .get_token(&[&self.storage_scope], None)
+            .await
+            .map_err(|e| object_store::Error::Generic {
+                store: "Azure",
+                source: Box::new(e),
+            })?;
 
         let mut state = self.state.lock().expect("Lock is not poisoned.");
         if state.is_none() {
