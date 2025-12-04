239dbfe06108f026406c02df5644ee2cb4cd246c
Jake Dern <jakedern@yahoo.com>
2025-12-04 14:19:17 -0800

    fix

--- rust/otap-dataflow/crates/otap/src/object_store/azure.rs
+++ rust/otap-dataflow/crates/otap/src/object_store/azure.rs
@@ -44,14 +44,15 @@ impl CredentialProvider for AzureTokenCredentialProvider {
     type Credential = AzureCredential;
 
     async fn get_credential(&self) -> object_store::Result<Arc<Self::Credential>> {
-        let maybe_new_token = self
-            .token_cred
-            .get_token(&[&self.storage_scope], None)
-            .await
-            .map_err(|e| object_store::Error::Generic {
-                store: "Azure",
-                source: Box::new(e),
-            })?;
+        let maybe_new_token = dbg!(
+            self.token_cred
+                .get_token(&[&self.storage_scope], None)
+                .await
+                .map_err(|e| object_store::Error::Generic {
+                    store: "Azure",
+                    source: Box::new(e),
+                })
+        )?;
 
         let mut state = self.state.lock().expect("Lock is not poisoned.");
         if state.is_none() {
@@ -67,10 +68,8 @@ impl CredentialProvider for AzureTokenCredentialProvider {
             return Ok(object_store_cred);
         }
 
-        let mut guard = self.state.lock().expect("Lock is not poisoned.");
-        let state = guard.as_mut().expect("State is initialized.");
+        let state = state.as_mut().expect("State is initialized.");
         let current_token = &state.current_access_token;
-
         let cred = if current_token.token.secret() == maybe_new_token.token.secret() {
             state.current_object_store_cred.clone()
         } else {
