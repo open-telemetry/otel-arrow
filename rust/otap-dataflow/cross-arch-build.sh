8e43370d28ef02c771037dda4b4b35805bfae0bc
Jake Dern <jakedern@yahoo.com>
2025-12-05 17:11:57 -0800

    c

--- rust/otap-dataflow/cross-arch-build.sh
+++ rust/otap-dataflow/cross-arch-build.sh
@@ -1,14 +1,18 @@
 set -exu
 
+if [ -z "${FEATURES:-}" ]; then
+  FEATURES=""
+fi
+
 if [ "${TARGETPLATFORM}" = "linux/amd64" ]; then
   RUST_TARGET=x86_64-unknown-linux-musl
   if [ "${TARGETPLATFORM}" != "${BUILDPLATFORM}" ]; then
-    apt-get update && apt-get install -y gcc-x86-64-linux-gnu
+    apt-get update && apt-get install -y gcc-x86-64-linux-musl
   fi
 elif [ "${TARGETPLATFORM}" = "linux/arm64" ]; then
   RUST_TARGET=aarch64-unknown-linux-musl
   if [ "${TARGETPLATFORM}" != "${BUILDPLATFORM}" ]; then
-    apt-get update && apt-get install -y gcc-aarch64-linux-gnu
+    apt-get update && apt-get install -y gcc-aarch64-linux-musl
   fi
 else
   echo "Unsupported target platform: ${TARGETPLATFORM}"
@@ -16,5 +20,5 @@ else
 fi
 
 rustup target add "${RUST_TARGET}"
-cargo build --release --target="${RUST_TARGET}"
+cargo build --release --features "$FEATURES" --target="${RUST_TARGET}"
 cp "target/${RUST_TARGET}/release/df_engine" .
