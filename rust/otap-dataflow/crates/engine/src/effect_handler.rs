caa0185bf251f4358eb1af00bdbb6fe4f534d2f5
cijothomas <cijo.thomas@gmail.com>
2025-12-11 10:21:40 -0800

    cf

--- rust/otap-dataflow/crates/engine/src/effect_handler.rs
+++ rust/otap-dataflow/crates/engine/src/effect_handler.rs
@@ -104,10 +104,7 @@ impl<PData> EffectHandlerCore<PData> {
         // (load balancing).
         // Goal: Load balancing incoming connections.
         // TODO: Investigate adding set_reuse_port support for Windows.
-        #[cfg(any(
-            target_os = "linux",
-            target_os = "macos"
-        ))]
+        #[cfg(any(target_os = "linux", target_os = "macos"))]
         {
             sock.set_reuse_port(true).map_err(into_engine_error)?;
         }
@@ -157,10 +154,7 @@ impl<PData> EffectHandlerCore<PData> {
         // (load balancing).
         // Goal: Load balancing incoming packets.
         // TODO: Investigate adding set_reuse_port support for Windows.
-        #[cfg(any(
-            target_os = "linux",
-            target_os = "macos"
-        ))]
+        #[cfg(any(target_os = "linux", target_os = "macos"))]
         {
             sock.set_reuse_port(true).map_err(into_engine_error)?;
         }
