afd561cc1c70ff088209cab608bac27436ebeac0
cijothomas <cijo.thomas@gmail.com>
2025-12-12 10:50:58 -0800

    one more

--- rust/otap-dataflow/crates/otap/src/otlp_exporter.rs
+++ rust/otap-dataflow/crates/otap/src/otlp_exporter.rs
@@ -1012,6 +1012,8 @@ mod tests {
         _ = shutdown_sender.send("Shutdown");
     }
 
+    // Skipping on Windows due to flakiness: https://github.com/open-telemetry/otel-arrow/issues/1611
+    #[cfg_attr(target_os = "windows", ignore)]
     #[test]
     fn test_receiver_not_ready_on_start_and_reconnect() {
         // the purpose of this test is to that the exporter behaves as expected in the face of
