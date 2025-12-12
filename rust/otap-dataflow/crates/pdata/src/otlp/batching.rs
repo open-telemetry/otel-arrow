dce6d9d601eb828eb0e509b443512287d2f651e1
Joshua MacDonald <josh.macdonald@gmail.com>
2025-12-12 12:46:16 -0800

    new file

---
+++ rust/otap-dataflow/crates/pdata/src/otlp/batching.rs
@@ -0,0 +1,17 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+use super::OtlpProtoBytes;
+use crate::error::{Error, Result};
+use otap_df_config::SignalType;
+use std::num::NonZeroU64;
+
+/// Combines OTLP content by concatenation of bytes. Because we have a
+/// top-level repeated field, this is precisely correct.
+pub fn make_bytes_batches(
+    signal: SignalType,
+    max_bytes: Option<NonZeroU64>,
+    records: Vec<OtlpProtoBytes>,
+) -> Result<Vec<OtlpProtoBytes>> {
+    Err(Error::EmptyBatch)
+}
