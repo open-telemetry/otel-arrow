7cffafe2cb2c3ac605852d8d87ba77b4a41b716c
Laurent Quérel <l.querel@f5.com>
2026-01-08 14:23:58 -0800

    Internal Telemetry Guidelines (#1727)
    This PR defines a set of guidelines for our internal telemetry and for
    describing how we can establish a telemetry by design process.
    
    Once this PR is merged, I will follow up with a series of PRs to align
    the existing instrumentation with these recommendations.
    
    ---------
    
    Co-authored-by: Cijo Thomas <cithomas@microsoft.com>

--- rust/otap-dataflow/crates/otap/src/transform_processor/metrics.rs
+++ rust/otap-dataflow/crates/otap/src/transform_processor/metrics.rs
@@ -7,17 +7,9 @@ use otap_df_telemetry::instrument::Counter;
 use otap_df_telemetry_macros::metric_set;
 
 /// Metrics for the TransformProcessor node.
-#[metric_set(name = "transform.processor.metrics")]
+#[metric_set(name = "transform.processor")]
 #[derive(Debug, Default, Clone)]
 pub struct Metrics {
-    /// PData messages consumed by this processor.
-    #[metric(unit = "{msg}")]
-    pub msgs_consumed: Counter<u64>,
-
-    /// PData messages forwarded by this processor.
-    #[metric(unit = "{msg}")]
-    pub msgs_forwarded: Counter<u64>,
-
     /// Number of messages successfully transformed.
     #[metric(unit = "{msg}")]
     pub msgs_transformed: Counter<u64>,
