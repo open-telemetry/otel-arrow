1fe606c694726d454abe2d04992c6c7d10a825a3
github-action-benchmark <github@users.noreply.github.com>
2026-01-08 23:00:18 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 7cffafe2cb2c3ac605852d8d87ba77b4a41b716c

--- docs/benchmarks/continuous-idle-state/data.js
+++ docs/benchmarks/continuous-idle-state/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767897796410,
+  "lastUpdate": 1767913217945,
   "repoUrl": "https://github.com/open-telemetry/otel-arrow",
   "entries": {
     "Benchmark": [
@@ -52,6 +52,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "idle_test_duration",
             "value": 15.000889,
+            "unit": "seconds",
+            "extra": "Continuous - Idle State Performance/Idle State Baseline - Idle Test Duration"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "l.querel@f5.com",
+            "name": "Laurent Quérel",
+            "username": "lquerel"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7cffafe2cb2c3ac605852d8d87ba77b4a41b716c",
+          "message": "Internal Telemetry Guidelines (#1727)\n\nThis PR defines a set of guidelines for our internal telemetry and for\ndescribing how we can establish a telemetry by design process.\n\nOnce this PR is merged, I will follow up with a series of PRs to align\nthe existing instrumentation with these recommendations.\n\n---------\n\nCo-authored-by: Cijo Thomas <cithomas@microsoft.com>",
+          "timestamp": "2026-01-08T22:23:58Z",
+          "tree_id": "23f046b4d5bf8c73ed3a81efab1b6611ecccbeb9",
+          "url": "https://github.com/open-telemetry/otel-arrow/commit/7cffafe2cb2c3ac605852d8d87ba77b4a41b716c"
+        },
+        "date": 1767913216997,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "idle_cpu_percentage_avg",
+            "value": 0.06261100518710624,
+            "unit": "%",
+            "extra": "Continuous - Idle State Performance/Idle State Baseline - Idle CPU % (Avg)"
+          },
+          {
+            "name": "idle_cpu_percentage_max",
+            "value": 0.08300101301332502,
+            "unit": "%",
+            "extra": "Continuous - Idle State Performance/Idle State Baseline - Idle CPU % (Max)"
+          },
+          {
+            "name": "idle_ram_mib_avg",
+            "value": 27.46875,
+            "unit": "MiB",
+            "extra": "Continuous - Idle State Performance/Idle State Baseline - Idle RAM (MiB) (Avg)"
+          },
+          {
+            "name": "idle_ram_mib_max",
+            "value": 27.6328125,
+            "unit": "MiB",
+            "extra": "Continuous - Idle State Performance/Idle State Baseline - Idle RAM (MiB) (Max)"
+          },
+          {
+            "name": "idle_test_duration",
+            "value": 15.00068,
             "unit": "seconds",
             "extra": "Continuous - Idle State Performance/Idle State Baseline - Idle Test Duration"
           }
