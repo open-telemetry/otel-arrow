07ae923ad4cbfbf66140b527e7b0ddcf9d8a3efc
Lalit Bhasin <lalit_fin@yahoo.com>
2026-02-26 16:02:08 -0800

    initial commit

---
+++ rust/otap-dataflow/crates/pdata-views/src/views/mod.rs
@@ -0,0 +1,9 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//! Backend-agnostic view traits for OTLP data traversal.
+
+pub mod common;
+pub mod logs;
+pub mod resource;
+pub mod trace;
--- rust/otap-dataflow/crates/pdata/src/views/resource.rs
+++ rust/otap-dataflow/crates/pdata/src/views/resource.rs
@@ -4,24 +4,4 @@
 //! This module contains backend-agnostic, zero-copy view traits for common OTLP messages from
 //! OTLP resources.proto
 
-use crate::views::common::AttributeView;
-
-/// View for Resource
-pub trait ResourceView {
-    /// The `AttributeView` trait associated with this impl of the `ResourceView` trait.
-    type Attribute<'att>: AttributeView
-    where
-        Self: 'att;
-
-    /// The associated iterator type for this impl of the the trait. The iterator will yield
-    /// borrowed references that must live as long as the lifetime 'att
-    type AttributesIter<'att>: Iterator<Item = Self::Attribute<'att>>
-    where
-        Self: 'att;
-
-    /// Access this resource's attributes
-    fn attributes(&self) -> Self::AttributesIter<'_>;
-
-    /// Access this resource's dropped attributes.The value is 0 when no attributes were dropped.
-    fn dropped_attributes_count(&self) -> u32;
-}
+pub use otap_pdata_views::views::resource::ResourceView;
