93f1bac533ade1abe96aceba725dee1fa36c743e
albertlockett <a.lockett@f5.com>
2026-04-14 16:07:18 -0400

    change date time parsing in OPL to use a tagged literal containing io 8601

--- rust/otap-dataflow/crates/opl/src/parser/temporal.rs
+++ rust/otap-dataflow/crates/opl/src/parser/temporal.rs
@@ -1,18 +1,22 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-use data_engine_expressions::date_utils::parse_date_time;
-use data_engine_expressions::{DateTimeScalarExpression, StaticScalarExpression, StringValue};
+use chrono::{DateTime, FixedOffset, NaiveDate, NaiveTime, TimeZone, Utc};
+use data_engine_expressions::{
+    DateTimeScalarExpression, QueryLocation, StaticScalarExpression, StringValue,
+};
 use data_engine_parser_abstractions::{
     ParserError, parse_standard_string_literal, to_query_location,
 };
 use pest::iterators::Pair;
 
-#[cfg(test)]
-use chrono::{DateTime, FixedOffset, NaiveDate, Utc};
-
 use crate::parser::invalid_child_rule_error;
 use crate::parser::{Rule, expression::no_inner_rule_error};
+
+/// Number of milliseconds in a nanosecond
+const MINUTES_PER_SECOND: i32 = 60;
+const SECONDS_PER_HOUR: i32 = 3600;
+const MILLIS_PER_NANO_SECOND: u32 = 1_000_000;
 
 pub(crate) fn parse_datetime_expression(
     datetime_expression_rule: Pair<'_, Rule>,
@@ -25,26 +29,12 @@ pub(crate) fn parse_datetime_expression(
     let rule_query_location = to_query_location(&rule);
 
     let datetime_value = match rule.as_rule() {
-        Rule::datetime_literal => {
-            let datetime_value = rule.as_str();
-            parse_date_time(datetime_value).map_err(|_e| {
-                ParserError::SyntaxError(
-                    rule_query_location.clone(),
-                    format!("Invalid datetime literal {datetime_value}"),
-                )
-            })?
-        }
         Rule::string_literal => {
             let static_str_scalar_expr = parse_standard_string_literal(rule);
             match static_str_scalar_expr {
                 StaticScalarExpression::String(str) => {
                     let str_value = str.get_value();
-                    parse_date_time(str_value).map_err(|_e| {
-                        ParserError::SyntaxError(
-                            rule_query_location.clone(),
-                            format!("Invalid datetime string literal {str_value:?}"),
-                        )
-                    })?
+                    parse_date_time(str_value, &rule_query_location)?
                 }
                 invalid_expr => {
                     return Err(ParserError::SyntaxError(
@@ -66,6 +56,78 @@ pub(crate) fn parse_datetime_expression(
     Ok(StaticScalarExpression::DateTime(
         DateTimeScalarExpression::new(rule_query_location, datetime_value),
     ))
+}
+
+fn parse_date_time(
+    str_value: &str,
+    query_location: &QueryLocation,
+) -> Result<DateTime<FixedOffset>, ParserError> {
+    let Ok(iso8601::DateTime {
+        date: parsed_date,
+        time: parsed_time,
+    }) = iso8601::datetime(str_value)
+    else {
+        return Err(ParserError::SyntaxError(
+            query_location.clone(),
+            format!("Invalid datetime literal {str_value:?}"),
+        ));
+    };
+
+    let naive_date = match parsed_date {
+        iso8601::Date::YMD { year, month, day } => NaiveDate::from_ymd_opt(year, month, day)
+            .ok_or_else(|| {
+                ParserError::SyntaxError(
+                    query_location.clone(),
+                    format!("Invalid date {parsed_date:?}"),
+                )
+            })?,
+        _ => {
+            todo!()
+        }
+    };
+    // Convert time
+    let naive_time = NaiveTime::from_hms_nano_opt(
+        parsed_time.hour,
+        parsed_time.minute,
+        parsed_time.second,
+        parsed_time.millisecond * MILLIS_PER_NANO_SECOND,
+    )
+    .ok_or_else(|| {
+        ParserError::SyntaxError(
+            query_location.clone(),
+            format!("Invalid time {parsed_date:?}"),
+        )
+    })?;
+
+    let naive_dt = naive_date.and_time(naive_time);
+
+    // Convert timezone
+    let dt = match parsed_time.tz_offset_hours {
+        0 if parsed_time.tz_offset_minutes == 0 => {
+            // UTC
+            Utc.from_utc_datetime(&naive_dt).fixed_offset()
+        }
+        _ => {
+            let offset_secs = parsed_time.tz_offset_hours * SECONDS_PER_HOUR
+                + parsed_time.tz_offset_minutes * MINUTES_PER_SECOND;
+            let offset = FixedOffset::east_opt(offset_secs).ok_or_else(|| {
+                ParserError::SyntaxError(
+                    query_location.clone(),
+                    format!("Invalid offset {offset_secs:?}"),
+                )
+            })?;
+            offset
+                .from_local_datetime(&naive_dt)
+                .single()
+                .ok_or_else(|| {
+                    ParserError::SyntaxError(
+                        query_location.clone(),
+                        format!("Invalid offset {naive_dt:?}"),
+                    )
+                })?
+        }
+    };
+    Ok(dt)
 }
 
 /// test helper for creating [`DateTime`]s
@@ -124,22 +186,10 @@ mod test {
     /// utility to which [`parse_datetime_expression`] delegates
     fn valid_test_cases() -> Vec<(&'static str, DateTime<FixedOffset>)> {
         vec![
-            // mid-endian (US) date format
-            ("02/04/2026", create_utc(2026, 2, 4, 0, 0, 0, 0)),
-            // mid-endian (US) date format with time
-            ("02/04/2026 5:30 AM", create_utc(2026, 2, 4, 5, 30, 0, 0)),
-            // ISO 8601
-            ("2026-02-04", create_utc(2026, 2, 4, 0, 0, 0, 0)),
             // ISO 8601 with time
             ("2026-02-04T05:30:00", create_utc(2026, 2, 4, 5, 30, 0, 0)),
-            // RFC 822
-            ("4 Feb 26 15:05", create_utc(2026, 2, 4, 15, 5, 0, 0)),
-            // RFC 822 with time and day of week
-            (
-                "Wed, 4 Feb 26 15:05:02 GMT",
-                create_utc(2026, 2, 4, 15, 5, 2, 0),
-            ),
-            // Explicit timezone offsets
+            // Explicit timezone offsets - for good measure test one  case during daylight savings
+            // and one case not during daylight savings ...
             (
                 "2026-02-04T05:30:00-05:00",
                 create_with_tz(2026, 2, 4, 5, 30, 0, 0, Canada::Eastern),
@@ -148,12 +198,6 @@ mod test {
                 "2026-07-31T11:15-04:00",
                 create_with_tz(2026, 7, 31, 11, 15, 0, 0, Canada::Eastern),
             ),
-            // TODO reenable this test case when TZ parsing bug fixed:
-            // https://github.com/open-telemetry/otel-arrow/issues/2047
-            // (
-            //     "Wed, 4 Feb 26 15:05:02 EST",
-            //     create_with_tz(2026, 02, 04, 15, 5, 2, 0, Canada::Eastern),
-            // ),
         ]
     }
 
@@ -177,31 +221,20 @@ mod test {
     #[test]
     fn test_parse_from_datetime_literal() {
         for (input, expected) in valid_test_cases() {
-            let expr = format!("datetime({input})");
+            let expr = format!("date_time\"{input}\"");
             run_test_success(&expr, expected);
         }
+    }
+
+    #[test]
+    fn test_parse_from_invalid_datetime_date_only() {
+        let err = run_test_failure("date_time\"2026-02-04\""); // not a valid datetime format
+        assert_eq!("Invalid datetime literal \"2026-02-04\"", err.to_string())
     }
 
     #[test]
     fn test_parse_from_invalid_datetime_literal() {
-        let err = run_test_failure("datetime(halloween)"); // not a valid date format
-        assert_eq!("Invalid datetime literal halloween", err.to_string())
-    }
-
-    #[test]
-    fn test_parse_from_string_literal() {
-        for (input, expected) in valid_test_cases() {
-            let expr = format!("datetime('{input}')");
-            run_test_success(&expr, expected);
-        }
-    }
-
-    #[test]
-    fn test_parse_from_invalid_string_literal() {
-        let err = run_test_failure("datetime('monday')"); // not a valid date format
-        assert_eq!(
-            "Invalid datetime string literal \"monday\"",
-            err.to_string()
-        )
+        let err = run_test_failure("date_time\"halloween\""); // not a valid datetime format
+        assert_eq!("Invalid datetime literal \"halloween\"", err.to_string())
     }
 }
