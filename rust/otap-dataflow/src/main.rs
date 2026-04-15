658e15120d0b8b6b48b4144b1ae77245fb8f9931
Saroj Kumar Patra <sapatrjv@gmail.com>
2026-04-14 17:47:12 -0700

    Enable Memory & CPU Profiling for df_engine

--- rust/otap-dataflow/src/main.rs
+++ rust/otap-dataflow/src/main.rs
@@ -51,10 +51,10 @@ use {
     std::sync::{LazyLock, Mutex},
 };
 
-#[cfg(feature = "mimalloc")]
+#[cfg(all(not(clippy), feature = "mimalloc"))]
 use mimalloc::MiMalloc;
 
-#[cfg(all(not(windows), feature = "jemalloc"))]
+#[cfg(all(not(clippy), not(windows), feature = "jemalloc"))]
 use tikv_jemallocator::Jemalloc;
 
 // -----------------------------------------------------------------------------
