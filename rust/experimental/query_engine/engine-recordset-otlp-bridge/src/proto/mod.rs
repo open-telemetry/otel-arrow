dc5667f0362bf24c66dcaebc0d2d2104b00ddd3a
Marc Snider <marcsn@pm.me>
2026-04-30 14:20:28 -0700

    Fixed problem from merge.

--- rust/experimental/query_engine/engine-recordset-otlp-bridge/src/proto/mod.rs
+++
@@ -1,9 +0,0 @@
-pub(crate) mod collector_logs;
-pub(crate) mod common;
-pub(crate) mod logs;
-pub(crate) mod resource;
-
-pub use collector_logs::*;
-pub use common::*;
-pub use logs::*;
-pub use resource::*;
