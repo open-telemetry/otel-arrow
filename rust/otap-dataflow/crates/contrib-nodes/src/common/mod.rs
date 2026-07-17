4ab1d242e454fc5eaacb68118e06ca37151b576a
c1ly <129437996+c1ly@users.noreply.github.com>
2026-07-17 16:40:28 -0700

    [otap-dataflow] add kafka exporter into contrib-nodes (#3262)
    # Change Summary
    
    Add Kafka Exporter implementation that takes inspiration from the go and
    rotel versions.
    
    Add kafka_util that shares common functions and data types with the
    kafka receiver and exporter
    
    ## What issue does this PR close?
    
    * Closes #3249 
    
    ## How are these changes tested?
    
    unit tests and integration tests with kafka broker (requires docker
    container)
    
    ## Are there any user-facing changes?
    
    no user face changes

---
+++ rust/otap-dataflow/crates/contrib-nodes/src/common/mod.rs
@@ -0,0 +1,8 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//! Shared functions and data types for contrib node implementations.
+
+/// Shared Kafka utilities for Kafka receiver and exporter.
+#[cfg(feature = "kafka-exporter")]
+pub mod kafka;
