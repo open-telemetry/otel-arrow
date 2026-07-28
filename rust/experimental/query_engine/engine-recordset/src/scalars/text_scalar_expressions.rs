666f62acd3bcfd4f6902cd8ba38f8bec9dc3deeb
Mikel Blanchard <mblanchard@macrosssoftware.com>
2026-07-28 16:53:42 -0700

    Lint.

--- rust/experimental/query_engine/engine-recordset/src/scalars/text_scalar_expressions.rs
+++ rust/experimental/query_engine/engine-recordset/src/scalars/text_scalar_expressions.rs
@@ -47,7 +47,7 @@ where
                             Value::String(v) => s.push_str(v.get_value()),
                             Value::Null => {
                                 // Note: Null becomes empty string
-                            },
+                            }
                             v => s.push_str(v.convert_to_string().as_ref()),
                         }
                     })?;
@@ -80,15 +80,17 @@ where
                     let mut s = String::new();
                     let mut len = 0;
 
-                    a.take((..).into(), |_, r| Ok(r), &mut |r: ResolvedValue<'_>| {
-                        match r.to_value() {
+                    a.take(
+                        (..).into(),
+                        |_, r| Ok(r),
+                        &mut |r: ResolvedValue<'_>| match r.to_value() {
                             Value::String(v) => {
                                 len += 1;
                                 if len > 1 {
                                     s.push_str(separator.as_ref());
                                 }
                                 s.push_str(v.get_value())
-                            },
+                            }
                             v => {
                                 len += 1;
                                 if len > 1 {
@@ -96,8 +98,8 @@ where
                                 }
                                 s.push_str(v.convert_to_string().as_ref());
                             }
-                        }
-                    })?;
+                        },
+                    )?;
 
                     ResolvedValue::Computed(OwnedValue::String(StringValueStorage::new(s)))
                 }
