a71939a7b6ea2ccffb0eea985c26054b8ad7a692
Dipanshu Singh <singhdipanshu133@gmail.com>
2026-08-04 11:11:32 +0530

    style: run cargo fmt

--- rust/otap-dataflow/crates/engine/src/context.rs
+++ rust/otap-dataflow/crates/engine/src/context.rs
@@ -893,7 +893,7 @@ impl ExtensionContext {
     #[test]
     fn register_node_channel_entity_includes_custom_attributes() {
         use otap_df_config::pipeline::telemetry::AttributeValue;
-        
+
         let registry = TelemetryRegistryHandle::new();
         let controller_ctx = ControllerContext::new_with_identity(
             registry.clone(),
@@ -901,7 +901,7 @@ impl ExtensionContext {
             "machine-abc",
             "container-xyz",
         );
-        
+
         let pipeline_params = PipelineContextParams {
             pipeline_group_id: Cow::Borrowed("group1"),
             pipeline_id: Cow::Borrowed("pipe1"),
@@ -910,20 +910,20 @@ impl ExtensionContext {
             thread_id: 0,
         };
         let base_pipeline_ctx = PipelineContext::new(controller_ctx, pipeline_params);
-        
+
         let mut custom_attrs = HashMap::new();
         let _ = custom_attrs.insert(
             "custom.identity.foo".to_string(),
             TelemetryAttribute::new(AttributeValue::String("bar".to_string())),
         );
-        
+
         let pipeline_ctx = base_pipeline_ctx.with_node_context(
             Cow::Borrowed("test-node"),
             NodeUrn::parse("urn:otel:receiver:test").unwrap(),
             NodeKind::Receiver,
             custom_attrs,
         );
-        
+
         let entity_key = pipeline_ctx.register_node_channel_entity(
             Cow::Borrowed("channel-1"),
             Cow::Borrowed("out"),
@@ -932,19 +932,24 @@ impl ExtensionContext {
             "test_type",
             "test_impl",
         );
-        
-        let has_custom_attr = registry.visit_entity(entity_key, |attrs| {
-            let mut found = false;
-            for (key, val) in attrs.iter_attributes() {
-                if key == "custom" {
-                    if format!("{:?}", val).contains("custom.identity.foo") {
-                        found = true;
+
+        let has_custom_attr = registry
+            .visit_entity(entity_key, |attrs| {
+                let mut found = false;
+                for (key, val) in attrs.iter_attributes() {
+                    if key == "custom" {
+                        if format!("{:?}", val).contains("custom.identity.foo") {
+                            found = true;
+                        }
                     }
                 }
-            }
-            found
-        }).unwrap_or(false);
-        
-        assert!(has_custom_attr, "The custom identity attribute should be present in the channel entity");
+                found
+            })
+            .unwrap_or(false);
+
+        assert!(
+            has_custom_attr,
+            "The custom identity attribute should be present in the channel entity"
+        );
     }
 }
