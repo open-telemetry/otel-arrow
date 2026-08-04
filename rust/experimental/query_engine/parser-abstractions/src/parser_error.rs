6f674a680b2e39c83522a24a751f285ddac057fc
albertlockett <a.lockett@f5.com>
2026-08-04 10:50:05 -0400

    added some additional test

--- rust/experimental/query_engine/parser-abstractions/src/parser_error.rs
+++ rust/experimental/query_engine/parser-abstractions/src/parser_error.rs
@@ -50,19 +50,22 @@ impl ParserError {
             pest::error::LineColLocation::Span(l, _) => l,
         };
 
-        // try to identify invalid syntax on the line that contains it.
-        let invalid_line = query.lines().nth(line - 1);
+        // try to identify invalid syntax on the line/column that contains it.
+        let invalid_line = query
+            .lines()
+            .nth(line - 1)
+            .and_then(|line| line.get(column - 1..));
 
         // if for some reason the error's line isn't in the query (which can happen for empty
         // queries, e.g. "", or if we're simply passed an error with a bad line), fall back to
         // identifying the invalid content by start..end range. If the range is also not valid
         // for the query, just fall back to using the query contents.
-        let invalid_content = invalid_line.or(query.get(start..end)).unwrap_or(query);
+        let content = invalid_line.or(query.get(start..end)).unwrap_or(query);
 
         Self::SyntaxNotSupported(
             QueryLocation::new(start, end, line, column)
                 .expect("QueryLocation could not be constructed"),
-            format!("Syntax '{invalid_content}' supplied in query is not supported"),
+            format!("Syntax '{content}' supplied in query is not supported"),
         )
     }
 }
@@ -81,22 +84,30 @@ mod test {
             ErrorVariant::CustomError {
                 message: "test".into(),
             },
-            Position::new("a\na", 2).unwrap(),
+            Position::new("a\nabc", 4).unwrap(),
         );
 
         // ensure we don't panic when the line doesn't exist
-        let (line, _) = match pest_error.line_col {
+        let (line, col) = match pest_error.line_col {
             LineColLocation::Pos(p) => p,
             _ => {
                 panic!("invalid pos")
             }
         };
         assert!(line > 0);
-        let error = ParserError::from_pest_error("abc", pest_error.clone());
+        assert!(col > 0);
+        let error = ParserError::from_pest_error("abcde", pest_error.clone());
         assert!(
             error
                 .to_string()
-                .contains("Syntax 'ab' supplied in query is not supported"),
+                .contains("Syntax 'abcd' supplied in query is not supported"),
+        );
+
+        let error = ParserError::from_pest_error("ab\nc", pest_error.clone());
+        assert!(
+            error
+                .to_string()
+                .contains("Syntax 'ab\nc' supplied in query is not supported"),
         );
 
         // ensure we don't panic when the position doesn't exist:
@@ -105,11 +116,11 @@ mod test {
             pest::error::InputLocation::Span(s) => s,
         };
         assert!(end > 1);
-        let error = ParserError::from_pest_error("a", pest_error);
+        let error = ParserError::from_pest_error("ab", pest_error);
         assert!(
             error
                 .to_string()
-                .contains("Syntax 'a' supplied in query is not supported"),
+                .contains("Syntax 'ab' supplied in query is not supported"),
         );
     }
 }
