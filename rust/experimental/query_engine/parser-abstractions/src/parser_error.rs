8937c8a8eb0a7619206d8c9b6dd27080c5f19cb3
albertlockett <a.lockett@f5.com>
2026-08-04 10:33:29 -0400

    add test

--- rust/experimental/query_engine/parser-abstractions/src/parser_error.rs
+++ rust/experimental/query_engine/parser-abstractions/src/parser_error.rs
@@ -37,6 +37,8 @@ impl From<&ExpressionError> for ParserError {
 }
 
 impl ParserError {
+    /// Converts the pest error to a syntax error, identifying the invalid content from the error
+    /// location information.
     pub fn from_pest_error(query: &str, pest_error: Error<impl pest::RuleType>) -> Self {
         let (start, end) = match pest_error.location {
             pest::error::InputLocation::Pos(p) => (0, p),
@@ -48,14 +50,66 @@ impl ParserError {
             pest::error::LineColLocation::Span(l, _) => l,
         };
 
-        let content = query.lines().nth(line - 1)
-            .or(query.get(start..end))
-            .unwrap_or(query);
+        // try to identify invalid syntax on the line that contains it.
+        let invalid_line = query.lines().nth(line - 1);
+
+        // if for some reason the error's line isn't in the query (which can happen for empty
+        // queries, e.g. "", or if we're simply passed an error with a bad line), fall back to
+        // identifying the invalid content by start..end range. If the range is also not valid
+        // for the query, just fall back to using the query contents.
+        let invalid_content = invalid_line.or(query.get(start..end)).unwrap_or(query);
 
         Self::SyntaxNotSupported(
             QueryLocation::new(start, end, line, column)
                 .expect("QueryLocation could not be constructed"),
-            format!("Syntax '{content}' supplied in query is not supported"),
+            format!("Syntax '{invalid_content}' supplied in query is not supported"),
         )
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use pest::Position;
+    use pest::error::{ErrorVariant, LineColLocation};
+
+    use super::*;
+
+    /// test to ensure we don't panic when receiving a pest error with an invalid location
+    #[test]
+    fn test_from_pest_error_invalid_location() {
+        let pest_error = Error::<()>::new_from_pos(
+            ErrorVariant::CustomError {
+                message: "test".into(),
+            },
+            Position::new("a\na", 2).unwrap(),
+        );
+
+        // ensure we don't panic when the line doesn't exist
+        let (line, _) = match pest_error.line_col {
+            LineColLocation::Pos(p) => p,
+            _ => {
+                panic!("invalid pos")
+            }
+        };
+        assert!(line > 0);
+        let error = ParserError::from_pest_error("abc", pest_error.clone());
+        assert!(
+            error
+                .to_string()
+                .contains("Syntax 'ab' supplied in query is not supported"),
+        );
+
+        // ensure we don't panic when the position doesn't exist:
+        let (_, end) = match pest_error.location {
+            pest::error::InputLocation::Pos(p) => (0, p),
+            pest::error::InputLocation::Span(s) => s,
+        };
+        assert!(end > 1);
+        let error = ParserError::from_pest_error("a", pest_error);
+        assert!(
+            error
+                .to_string()
+                .contains("Syntax 'a' supplied in query is not supported"),
+        );
     }
 }
