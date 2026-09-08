ce66bb28843cafd17f3d8a19bf0cd960dbb5b476
Mikel Blanchard <mblanchard@macrosssoftware.com>
2026-09-08 15:08:18 -0700

    WIP.

--- rust/otap-dataflow/crates/core-nodes/src/exporters/otlp_grpc_exporter/mod.rs
+++ rust/otap-dataflow/crates/core-nodes/src/exporters/otlp_grpc_exporter/mod.rs
@@ -176,7 +176,7 @@ impl OTLPExporter {
             metrics,
             auth_provider: new_http_client_auth_provider(
                 capabilities,
-                HttpClientAuthProviders::BEARER_TOKEN | HttpClientAuthProviders::API_KEY,
+                HttpClientAuthProviders::BEARER_TOKEN | HttpClientAuthProviders::API_KEY | HttpClientAuthProviders::BASIC,
             )?,
         })
     }
