1096c58a94772fd7e432e9debdb9950b1d169a9d
github-action-benchmark <github@users.noreply.github.com>
2026-09-09 14:29:07 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for e6ed225725885e98f85db86bc7cfd978746eff4f

--- docs/benchmarks/binary-size/data.js
+++ docs/benchmarks/binary-size/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788917442350,
+  "lastUpdate": 1788964147593,
   "repoUrl": "https://github.com/open-telemetry/otel-arrow",
   "entries": {
     "Benchmark": [
@@ -34049,6 +34049,150 @@ window.BENCHMARK_DATA = {
           {
             "name": "linux-arm64-binary-size",
             "value": 102.6,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "a.lockett@f5.com",
+            "name": "albertlockett",
+            "username": "albertlockett"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e6ed225725885e98f85db86bc7cfd978746eff4f",
+          "message": "feat(controller): opamp controller extension support mTLS (#4007)\n\n# Change summary\n\nAdds support for the opamp controller extension to use TLS.\n\nAdds a new section to the config under the heading `tls`, which is an\n[`Option<TlsCilentConfig>`](https://github.com/open-telemetry/otel-arrow/blob/c3d57ae7d36212490932e2ba7665f5b55aa52aae/rust/otap-dataflow/crates/config/src/tls.rs#L55)\n(same config used for otlp grpc/http exporters), used to configure\nTLS/mTLS.\n\nWhen using TLS, the URL scheme must be `wss://`.\n\nThe `validate_config` function now validates that the config is valid --\ne.g. that the files it points at exist (if configuring certs/keys using\nfiles), and that the certs are valid pems.\n\nMost of new code is in a new module called `tls.rs` which takes the\nextension config and creates a `rustls::ClientConfig`, which the\ncontroller then uses to create the TLS variant of tokio tungestenite's\n`Connector` enum and uses this when connecting server.\n\nAdds tests for happy path TLS/mTLS & various failure scenarios.\n\nAdd example config for using TLS/mTLS and updates README to mention this\nconfig.\n\n\n## Related issue\n\n<!--We highly recommend correlation of every PR to an issue-->\n\n* Closes #3884\n\n## Validation\n\n<!--How did you confirm your change has the intended effect?-->\n\nUnit tests\n\nI also validated the changes using the example opamp server in opamp-go\n\n## User-facing changes\n\n<!--\nDescribe the impact, or write `None`.\nUser-facing changes require a `.chloggen/*.yaml` entry. If no entry is\nneeded,\ninclude `chore` in the PR title. Documentation-only changes are exempt.\n-->\n\nYes - these new tls options are user facing config",
+          "timestamp": "2026-09-09T13:39:23Z",
+          "tree_id": "b5eaaf57e2198cc589734b96c0e8d5c1cdf97a16",
+          "url": "https://github.com/open-telemetry/otel-arrow/commit/e6ed225725885e98f85db86bc7cfd978746eff4f"
+        },
+        "date": 1788964131994,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "linux-amd64-text-size",
+            "value": 83.55,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-amd64-crate-std",
+            "value": 4.73,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-amd64-crate-otel_arrow_dfe_core_nodes",
+            "value": 3.99,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-amd64-crate-arrow_array",
+            "value": 3.68,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-amd64-crate-datafusion_expr",
+            "value": 3.53,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-amd64-crate-datafusion_functions_aggregate",
+            "value": 3.04,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-amd64-crate-datafusion_common",
+            "value": 3.01,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-amd64-crate-arrow_cast",
+            "value": 3,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-amd64-crate-[Unknown]",
+            "value": 2.98,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-amd64-crate-datafusion_physical_plan",
+            "value": 2.92,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-amd64-crate-otel_arrow_dfe_query_engine",
+            "value": 2.7,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-arm64-text-size",
+            "value": 70.95,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-arm64-crate-std",
+            "value": 4.81,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-arm64-crate-arrow_array",
+            "value": 3.51,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-arm64-crate-otel_arrow_dfe_core_nodes",
+            "value": 3.46,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-arm64-crate-datafusion_expr",
+            "value": 3.17,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-arm64-crate-datafusion_common",
+            "value": 2.75,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-arm64-crate-arrow_cast",
+            "value": 2.49,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-arm64-crate-datafusion_physical_plan",
+            "value": 2.49,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-arm64-crate-datafusion_functions_aggregate",
+            "value": 2.46,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-arm64-crate-[Unknown]",
+            "value": 2.41,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-arm64-crate-otel_arrow_dfe_query_engine",
+            "value": 2.06,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-amd64-binary-size",
+            "value": 115.59,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-arm64-binary-size",
+            "value": 102.98,
             "unit": "MB"
           }
         ]
